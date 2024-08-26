0831298648d5db3ff053f8b9f0d058b1c64a8f7f
Justin Brooks <jsbroks@gmail.com>
2024-08-25 22:39:02 -0400

    fix formatting

--- packages/node-sdk/src/models/index.ts
+++ packages/node-sdk/src/models/index.ts
@@ -1,13 +1,13 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './AcknowledgeJob200Response';
-export * from './GetAgentRunningExecutions200ResponseInner';
-export * from './GetJobExecution200Response';
-export * from './GetNextJobs200Response';
-export * from './GetNextJobs200ResponseJobExecutionsInner';
-export * from './SetTargetProvidersTargetsRequest';
-export * from './SetTargetProvidersTargetsRequestTargetsInner';
-export * from './UpdateJobAgent200Response';
-export * from './UpdateJobAgentRequest';
-export * from './UpdateJobExecution200Response';
-export * from './UpdateJobExecutionRequest';
+export * from "./AcknowledgeJob200Response";
+export * from "./GetAgentRunningExecutions200ResponseInner";
+export * from "./GetJobExecution200Response";
+export * from "./GetNextJobs200Response";
+export * from "./GetNextJobs200ResponseJobExecutionsInner";
+export * from "./SetTargetProvidersTargetsRequest";
+export * from "./SetTargetProvidersTargetsRequestTargetsInner";
+export * from "./UpdateJobAgent200Response";
+export * from "./UpdateJobAgentRequest";
+export * from "./UpdateJobExecution200Response";
+export * from "./UpdateJobExecutionRequest";
