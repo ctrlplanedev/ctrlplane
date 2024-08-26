00b24d1ffabfb56a4d6414d1afeed898605ed8d4
Justin Brooks <jsbroks@gmail.com>
2024-08-25 22:09:55 -0400

    add google cloud gke sync

--- providers/google-cloud/compute-scanner/src/index.ts
+++ providers/google-cloud/compute-scanner/src/index.ts
@@ -3,10 +3,45 @@ import { CronJob } from "cron";
 import { logger } from "@ctrlplane/logger";
 
 import { env } from "./config.js";
+import {
+  getKubernetesClusters,
+  getKubernetesNamespace,
+  gkeLogger,
+} from "./gke.js";
+import { api } from "./sdk.js";
+
+const getScannerId = async () => {
+  try {
+    const { id } = await api.upsertTargetProvider({
+      workspace: env.CTRLPLANE_WORKSPACE,
+      name: env.CTRLPLANE_SCANNER_NAME,
+    });
+    return id;
+  } catch (error) {
+    logger.error(error);
+    logger.error(
+      `Failed to get scanner ID. This could be caused by incorrect workspace (${env.CTRLPLANE_WORKSPACE}), or API Key`,
+      { error },
+    );
+  }
+  return null;
+};
 
 const scan = async () => {
+  const id = await getScannerId();
+  if (id == null) return;
+
+  logger.info(`Scanner ID: ${id}`, { id });
   logger.info("Running google compute scanner", {
     date: new Date().toISOString(),
+  });
+
+  const targets = await getKubernetesClusters();
+  gkeLogger.info(`Found ${targets.length} clusters`, { count: targets.length });
+
+  const namespaces = await getKubernetesNamespace(targets);
+  gkeLogger.info(`Found ${namespaces.length} namespaces`, {
+    count: namespaces.length,
   });
 };
 
