21a3a8e54c19b0fbd6916cfa87468580050d903f
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-08-27 13:59:47 -0500

    make destructive button more visable and use react copyToClipboard

--- packages/ui/src/copy-button.tsx
+++ packages/ui/src/copy-button.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { TbCopy } from "react-icons/tb";
+import { useCopyToClipboard } from "react-use";
 
 import { Button } from "./button";
 
@@ -7,17 +8,17 @@ export const CopyButton: React.FC<{ textToCopy: string }> = ({
   textToCopy,
 }) => {
   const [copied, setCopied] = useState(false);
+  const [, copyToClipboard] = useCopyToClipboard();
 
-  const handleCopy = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    await navigator.clipboard.writeText(textToCopy);
+  const handleCopy = () => {
+    copyToClipboard(textToCopy);
     setCopied(true);
-    setTimeout(() => setCopied(false), 2000); // Reset the copied state after 2 seconds
+    setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
   };
 
   return (
     <Button variant="outline" onClick={handleCopy} type="button">
-      {copied ? "Copied!" : "Copy ID"}
+      {copied ? "Copied" : "Copy"}
       <TbCopy className="ml-2" />
     </Button>
   );
