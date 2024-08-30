e9d8abcd23e255f25575117fe3ed258b47fd319a
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-08-29 22:43:14 -0500

    fix: Setup job-execution-sync-worker

--- apps/event-worker/src/index.ts
+++ apps/event-worker/src/index.ts
@@ -1,17 +1,21 @@
 import { logger } from "@ctrlplane/logger";
 
 import { createDispatchExecutionJobWorker } from "./job-execution-dispatch/index.js";
+import { createJobExecutionSyncWorker } from "./job-execution-sync/index.js";
 import { redis } from "./redis.js";
 import { createTargetScanWorker } from "./target-scan/index.js";
 
 const targetScanWorker = createTargetScanWorker();
+const jobExecutionSyncWorker = createJobExecutionSyncWorker();
 const dispatchExecutionJobWorker = createDispatchExecutionJobWorker();
 
 const shutdown = () => {
   logger.warn("Exiting...");
 
   targetScanWorker.close();
+  jobExecutionSyncWorker.close();
   dispatchExecutionJobWorker.close();
+
   redis.quit();
 
   process.exit(0);
