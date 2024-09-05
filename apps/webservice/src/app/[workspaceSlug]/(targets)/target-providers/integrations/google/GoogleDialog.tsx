91e37bec237b35eaa46083dc60f3dbdfe886effb
Aditya Choudhari <48932219+adityachoudhari26@users.noreply.github.com>
2024-09-05 13:16:33 -0700

    fix: Service account ui  (#16)

--- apps/webservice/src/app/[workspaceSlug]/(targets)/target-providers/integrations/google/GoogleDialog.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(targets)/target-providers/integrations/google/GoogleDialog.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useState } from "react";
+import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useFieldArray, useForm } from "react-hook-form";
+import { TbBulb, TbCheck, TbCopy } from "react-icons/tb";
+import { useCopyToClipboard } from "react-use";
 import { z } from "zod";
 
 import { cn } from "@ctrlplane/ui";
@@ -25,6 +29,7 @@ import {
   FormMessage,
 } from "@ctrlplane/ui/form";
 import { Input } from "@ctrlplane/ui/input";
+import { Label } from "@ctrlplane/ui/label";
 
 import { api } from "~/trpc/react";
 
@@ -49,6 +54,16 @@ export const GoogleDialog: React.FC<{ children: React.ReactNode }> = ({
     name: "projectIds",
     control: form.control,
   });
+
+  const [isCopied, setIsCopied] = useState(false);
+  const [, copy] = useCopyToClipboard();
+  const handleCopy = () => {
+    copy(workspace.data?.googleServiceAccountEmail ?? "");
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
 
   const router = useRouter();
   const utils = api.useUtils();
@@ -75,7 +90,51 @@ export const GoogleDialog: React.FC<{ children: React.ReactNode }> = ({
                 Google provider allows you to configure and import GKE clusters
                 from google.
               </DialogDescription>
+
+              <div
+                className="relative mb-4 flex w-fit items-center gap-2 rounded-md bg-neutral-800/50 px-4 py-3 text-muted-foreground"
+                role="alert"
+              >
+                <TbBulb className="h-6 w-6 flex-shrink-0" />
+                <span className="text-sm">
+                  To use the Google provider, you will need to invite our
+                  service account to your project and configure the necessary
+                  permissions. Read more{" "}
+                  <Link
+                    href="https://docs.ctrlplane.dev/integrations/google-cloud/compute-scanner"
+                    target="_blank"
+                    className="underline"
+                  >
+                    here
+                  </Link>
+                  .
+                </span>
+              </div>
             </DialogHeader>
+
+            <div className="space-y-2">
+              <Label>Service Account</Label>
+              <div className="relative flex items-center">
+                <Input
+                  value={workspace.data?.googleServiceAccountEmail ?? ""}
+                  className="disabled:cursor-default"
+                  disabled
+                />
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  type="button"
+                  onClick={handleCopy}
+                  className="absolute right-2 h-4 w-4 bg-neutral-950 backdrop-blur-sm transition-all hover:bg-neutral-950 focus-visible:ring-0"
+                >
+                  {isCopied ? (
+                    <TbCheck className="h-4 w-4 bg-neutral-950 text-green-500" />
+                  ) : (
+                    <TbCopy className="h-4 w-4" />
+                  )}
+                </Button>
+              </div>
+            </div>
 
             <FormField
               control={form.control}
--- apps/webservice/src/app/[workspaceSlug]/settings/(settings)/workspace/integrations/(integration)/google/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/settings/(settings)/workspace/integrations/(integration)/google/page.tsx
@@ -1,30 +1,11 @@
-import { SiGooglecloud } from "react-icons/si";
-
-import { Card } from "@ctrlplane/ui/card";
+import { GoogleIntegration } from "./GoogleIntegration";
 
 export const metadata = { title: "Google Integrations - Settings" };
 
-export default function GoogleIntegrationPage() {
-  return (
-    <div className="flex flex-col gap-12">
-      <div className="flex items-center gap-4">
-        <SiGooglecloud className="h-14 w-14 text-red-400" />
-        <div className="flex flex-col gap-1">
-          <h1 className="text-3xl font-bold">Google</h1>
-          <p className="text-sm text-muted-foreground">
-            Sync deployment targets, trigger google workflows and more.
-          </p>
-        </div>
-      </div>
-
-      <Card className="flex items-center justify-between rounded-md p-4">
-        <div className="space-y-1">
-          <h2 className="text-lg font-semibold">Google Cloud</h2>
-          <p className="text-sm text-muted-foreground">
-            Sync deployment targets, trigger google workflows and more.
-          </p>
-        </div>
-      </Card>
-    </div>
-  );
+export default function GoogleIntegrationPage({
+  params,
+}: {
+  params: { workspaceSlug: string };
+}) {
+  return <GoogleIntegration workspaceSlug={params.workspaceSlug} />;
 }
