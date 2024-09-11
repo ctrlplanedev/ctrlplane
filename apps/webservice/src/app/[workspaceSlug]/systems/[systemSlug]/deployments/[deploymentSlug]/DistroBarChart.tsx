d3a8b55c70f74aa4c01c1a303bdd5a8bb2d3c09f
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-09-10 17:28:11 -0700

    clean

--- apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/[deploymentSlug]/DistroBarChart.tsx
+++ apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/[deploymentSlug]/DistroBarChart.tsx
@@ -11,25 +11,23 @@ export const DistroBarChart: React.FC<{
     version: string;
     count: number;
   }>;
-}> = ({ distro, distroPadding }) => {
-  return (
-    <ResponsiveContainer width="100%" height={250}>
-      <BarChart
-        data={[...distro, ...distroPadding]}
-        margin={{ bottom: -40, top: 30, right: 20, left: -10 }}
-      >
-        <XAxis
-          dataKey="version"
-          type="category"
-          interval={0}
-          height={100}
-          style={{ fontSize: "0.75rem" }}
-          angle={-45}
-          textAnchor="end"
-        />
-        <YAxis style={{ fontSize: "0.75rem" }} dataKey="count" />
-        <Bar dataKey="count" fill="#8884d8"></Bar>
-      </BarChart>
-    </ResponsiveContainer>
-  );
-};
+}> = ({ distro, distroPadding }) => (
+  <ResponsiveContainer width="100%" height={250}>
+    <BarChart
+      data={[...distro, ...distroPadding]}
+      margin={{ bottom: -40, top: 30, right: 20, left: -10 }}
+    >
+      <XAxis
+        dataKey="version"
+        type="category"
+        interval={0}
+        height={100}
+        style={{ fontSize: "0.75rem" }}
+        angle={-45}
+        textAnchor="end"
+      />
+      <YAxis style={{ fontSize: "0.75rem" }} dataKey="count" />
+      <Bar dataKey="count" fill="#8884d8"></Bar>
+    </BarChart>
+  </ResponsiveContainer>
+);
