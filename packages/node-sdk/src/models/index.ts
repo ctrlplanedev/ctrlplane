6f0b4a99a5f844d1a02917d1752130c1fd72b243
Justin Brooks <jsbroks@gmail.com>
2024-09-16 16:11:33 -0400

    cleanup nodesdk

--- packages/node-sdk/src/models/index.ts
+++ packages/node-sdk/src/models/index.ts
@@ -1,13 +1,13 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from "./AcknowledgeJob200Response";
-export * from "./GetAgentRunningJob200ResponseInner";
-export * from "./GetJob200Response";
-export * from "./GetNextJobs200Response";
-export * from "./GetNextJobs200ResponseJobsInner";
-export * from "./SetTargetProvidersTargetsRequest";
-export * from "./SetTargetProvidersTargetsRequestTargetsInner";
-export * from "./UpdateJob200Response";
-export * from "./UpdateJobAgent200Response";
-export * from "./UpdateJobAgentRequest";
-export * from "./UpdateJobRequest";
+export * from './AcknowledgeJob200Response';
+export * from './GetAgentRunningJob200ResponseInner';
+export * from './GetJob200Response';
+export * from './GetNextJobs200Response';
+export * from './GetNextJobs200ResponseJobsInner';
+export * from './SetTargetProvidersTargetsRequest';
+export * from './SetTargetProvidersTargetsRequestTargetsInner';
+export * from './UpdateJob200Response';
+export * from './UpdateJobAgent200Response';
+export * from './UpdateJobAgentRequest';
+export * from './UpdateJobRequest';
