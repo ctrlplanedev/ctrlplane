019cec76465ef1aa2df35a06126acb413dffdfc9
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-09-15 23:19:44 -0700

    revert

--- packages/job-dispatch/src/job-variables.ts
+++ packages/job-dispatch/src/job-variables.ts
@@ -23,7 +23,7 @@ export const createVariables = async (tx: Tx, jobId: string): Promise<void> => {
   const job = await tx
     .select()
     .from(SCHEMA.job)
-    .innerJoin(
+    .leftJoin(
       SCHEMA.releaseJobTrigger,
       eq(SCHEMA.releaseJobTrigger.jobId, SCHEMA.job.id),
     )
@@ -45,11 +45,21 @@ export const createVariables = async (tx: Tx, jobId: string): Promise<void> => {
   if (job == null) throw new Error(`Job with id ${jobId} not found`);
 
   const jobVariables: SCHEMA.JobVariable[] =
-    await determineVariablesForReleaseJob(tx, {
-      ...job.job,
-      releaseJobTrigger: job.release_job_trigger,
-    });
-
+    job.release_job_trigger != null
+      ? await determineVariablesForReleaseJob(tx, {
+          ...job.job,
+          releaseJobTrigger: job.release_job_trigger,
+        })
+      : job.runbook_job_trigger != null
+        ? await determineVariablesForRunbookJob(tx)
+        : [];
   if (jobVariables.length > 0)
     await tx.insert(SCHEMA.jobVariable).values(jobVariables);
+};
+
+const determineVariablesForRunbookJob = async (
+  _: Tx,
+  // eslint-disable-next-line @typescript-eslint/require-await
+): Promise<SCHEMA.JobVariable[]> => {
+  throw new Error("not implemented");
 };
