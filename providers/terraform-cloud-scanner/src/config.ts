47010e534e0add2ac0cfdadb216b063bac10d616
Zachary Blasczyk <77289967+zacharyblasczyk@users.noreply.github.com>
2024-09-18 00:20:32 -0500

    refactor (#39)
    Signed-off-by: Justin Brooks <jsbroks@gmail.com>
    Co-authored-by: Justin Brooks <jsbroks@gmail.com>

--- providers/terraform-cloud-scanner/src/config.ts
+++ providers/terraform-cloud-scanner/src/config.ts
@@ -20,7 +20,7 @@ export const env = createEnv({
 
     CONCURRENT_REQUESTS: z.number().default(10),
 
-    CRON_ENABLED: z.boolean().default(true),
+    CRON_ENABLED: z.string().default("true"),
     CRON_TIME: z.string().default("*/5 * * * *"),
   },
   runtimeEnv: process.env,
