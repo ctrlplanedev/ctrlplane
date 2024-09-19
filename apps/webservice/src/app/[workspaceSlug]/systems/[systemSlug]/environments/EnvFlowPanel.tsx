289114202d2381e1930870543b6ca697163f03b2
Justin Brooks <jsbroks@gmail.com>
2024-09-19 11:09:48 -0400

    removed use filter logic in add env

--- apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/environments/EnvFlowPanel.tsx
+++ apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/environments/EnvFlowPanel.tsx
@@ -2,13 +2,12 @@
 
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useFieldArray, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { TbFilter, TbLayout, TbPlant, TbResize, TbTrash } from "react-icons/tb";
 import { MarkerType, useReactFlow, useViewport } from "reactflow";
 import colors from "tailwindcss/colors";
 import { z } from "zod";
 
-import { cn } from "@ctrlplane/ui";
 import { Button } from "@ctrlplane/ui/button";
 import {
   Dialog,
@@ -39,15 +38,13 @@ import { usePanel } from "./SidepanelContext";
 const environmentForm = z.object({
   name: z.string(),
   description: z.string().default(""),
-  targetFilter: z.array(z.object({ key: z.string(), value: z.string() })),
 });
 
 type EnvironmentFormValues = z.infer<typeof environmentForm>;
 
 const AddEnvironmentButton: React.FC<{
-  workspaceId: string;
   systemId: string;
-}> = ({ workspaceId, systemId }) => {
+}> = ({ systemId }) => {
   const create = api.environment.create.useMutation();
 
   const { setSelectedNodeId } = usePanel();
@@ -57,26 +54,7 @@ const AddEnvironmentButton: React.FC<{
     defaultValues: {
       name: "",
       description: "",
-      targetFilter: [],
     },
-  });
-
-  const { targetFilter } = form.watch();
-  const targets = api.target.byWorkspaceId.list.useQuery({
-    workspaceId, // Assuming systemId is the workspaceId
-    filters: [
-      {
-        key: "labels",
-        value: Object.fromEntries(
-          targetFilter.map(({ key, value }) => [key, value]),
-        ),
-      },
-    ],
-  });
-
-  const { fields } = useFieldArray({
-    name: "targetFilter",
-    control: form.control,
   });
 
   const { addNodes, addEdges } = useReactFlow();
@@ -87,13 +65,6 @@ const AddEnvironmentButton: React.FC<{
     const env = await create.mutateAsync({
       ...values,
       systemId,
-      targetFilter: {
-        operator: "and",
-        conditions: values.targetFilter.map(({ key, value }) => ({
-          label: key,
-          value,
-        })),
-      },
     });
     addNodes({
       id: env.id,
@@ -167,40 +138,6 @@ const AddEnvironmentButton: React.FC<{
               )}
             />
 
-            <div>
-              {fields.map((field, index) => (
-                <FormField
-                  control={form.control}
-                  key={field.id}
-                  name={`targetFilter.${index}`}
-                  render={({ field: { onChange, value } }) => (
-                    <FormItem>
-                      <FormLabel className={cn(index !== 0 && "sr-only")}>
-                        Target Filter ({targets.data?.total ?? "-"})
-                      </FormLabel>
-                      <FormControl>
-                        <div className="flex items-center gap-2">
-                          <Input
-                            placeholder="Key"
-                            value={value.key}
-                            onChange={(e) =>
-                              onChange({ ...value, key: e.target.value })
-                            }
-                          />
-                          <Input
-                            placeholder="Value"
-                            value={value.value}
-                            onChange={(e) =>
-                              onChange({ ...value, value: e.target.value })
-                            }
-                          />
-                        </div>
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              ))}
-            </div>
             <DialogFooter>
               <Button type="submit">Create</Button>
             </DialogFooter>
@@ -274,10 +211,9 @@ const useDeleteNodeOrEdge = () => {
 };
 
 export const EnvFlowPanel: React.FC<{
-  workspaceId: string;
   systemId: string;
   onLayout: () => void;
-}> = ({ onLayout, workspaceId, systemId }) => {
+}> = ({ onLayout, systemId }) => {
   const { fitView } = useReactFlow();
   const { disabled, onDelete } = useDeleteNodeOrEdge();
   return (
@@ -303,7 +239,7 @@ export const EnvFlowPanel: React.FC<{
         <Separator orientation="vertical" className="h-10" />
       </div>
 
-      <AddEnvironmentButton workspaceId={workspaceId} systemId={systemId} />
+      <AddEnvironmentButton systemId={systemId} />
       <NewPolicyButton systemId={systemId} />
 
       <div className="px-2">
