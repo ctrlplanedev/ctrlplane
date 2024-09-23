47fc1650ee2b40fd88049ac73784fbe0ac377191
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-09-23 12:21:01 -0500

    fix

--- apps/webservice/src/instrumentation.ts
+++ apps/webservice/src/instrumentation.ts
@@ -2,10 +2,8 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { PgInstrumentation } from "@opentelemetry/instrumentation-pg";
 import { registerOTel } from "@vercel/otel";
 
-import { env } from "~/env";
-
 export async function register() {
-  if (env.NEXT_RUNTIME === "nodejs") await import("./instrumentation");
+  await import("./instrumentation");
   registerOTel({ serviceName: "ctrlplane/webservice" });
   registerInstrumentations({
     instrumentations: [
