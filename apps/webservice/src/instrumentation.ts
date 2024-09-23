bc55a0a2b75064d2528e221b06f754672cb94d63
Zachary Blasczyk <77289967+zacharyblasczyk@users.noreply.github.com>
2024-09-23 12:47:55 -0500

    feat: PG instrumentation (#63)

--- apps/webservice/src/instrumentation.ts
+++ apps/webservice/src/instrumentation.ts
@@ -1,5 +1,17 @@
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { PgInstrumentation } from "@opentelemetry/instrumentation-pg";
 import { registerOTel } from "@vercel/otel";
 
-export function register() {
+export async function register() {
+  await import("./instrumentation");
   registerOTel({ serviceName: "ctrlplane/webservice" });
+  registerInstrumentations({
+    instrumentations: [
+      new PgInstrumentation({
+        enhancedDatabaseReporting: true,
+        enabled: true,
+        addSqlCommenterCommentToQueries: true,
+      }),
+    ],
+  });
 }
