9224d9ed14ca87ffd7580728d6055171587776b5
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-09-25 02:03:07 -0500

    WIP needs review

--- integrations/fetch-job-inputs/src/index.ts
+++ integrations/fetch-job-inputs/src/index.ts
@@ -5,6 +5,9 @@ import { Configuration, DefaultApi } from "@ctrlplane/node-sdk";
 const config = new Configuration({
   basePath: core.getInput("api_url", { required: true }) + "/api",
   apiKey: core.getInput("api_key", { required: true }),
+  // basePath: "http://localhost:3000/api",
+  // apiKey:
+  //   "f5116107b520fb09.fa1421041eaeee226a384cc840b39773d89154ed48b4903380fc100edbab273d",
 });
 
 const api = new DefaultApi(config);
@@ -12,23 +15,26 @@ const api = new DefaultApi(config);
 const setOutputsRecursively = (prefix: string, obj: any) => {
   if (typeof obj === "object" && obj !== null) {
     for (const [key, value] of Object.entries(obj)) {
-      const newPrefix = prefix ? `${prefix}_${key}` : key;
+      const sanitizedKey = key.split(".").join("_");
+      const newPrefix = prefix ? `${prefix}_${sanitizedKey}` : sanitizedKey;
       if (typeof value === "object" && value !== null) {
         setOutputsRecursively(newPrefix, value);
       } else {
         core.info(`${newPrefix}: ${String(value)}`);
         core.setOutput(newPrefix, value);
+        console.log(`${newPrefix}: ${String(value)}`);
       }
     }
   } else {
     core.info(`${prefix}: ${String(obj)}`);
     core.setOutput(prefix, obj);
+    console.log(`${prefix}: ${String(obj)}`);
   }
 };
 
 async function run() {
   const jobId = core.getInput("job_id", { required: true });
-
+  // const jobId = "3953a1ea-3a41-4515-9fa5-809bc03cb51d";
   await api
     .getJob({ jobId })
     .then((response) => {
@@ -42,11 +48,16 @@ async function run() {
       core.info(`Environment name: ${environment?.name}`);
       core.info(`Release version: ${release?.version}`);
 
+      // console.log(`Target name: ${target?.name}`);
+      // console.log(`Environment name: ${environment?.name}`);
+      // console.log(`Release version: ${release?.version}`);
+
       setOutputsRecursively("config", config);
       setOutputsRecursively("variable", variables ?? {});
     })
     .catch((error) => {
       core.setFailed(`Action failed: ${error.message}`);
+      // console.error(`Action failed: ${error.message}`);
     });
 }
 
