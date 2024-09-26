e64ab13dcccf92a8d81c4f7e33d223d7284f6bb7
Justin Brooks <jsbroks@gmail.com>
2024-09-26 01:20:25 -0400

    scroll on sidebar

--- apps/webservice/src/app/[workspaceSlug]/SidebarPanels.tsx
+++ apps/webservice/src/app/[workspaceSlug]/SidebarPanels.tsx
@@ -26,11 +26,13 @@ export const SidebarPanels: React.FC<{
         className="min-w-[220px] max-w-[300px] bg-black"
         defaultSize={10}
       >
-        {isSettingsPage ? (
-          <SidebarSettings workspaceSlug={workspace.slug} />
-        ) : (
-          <SidebarMain workspace={workspace} systems={systems} />
-        )}
+        <div className="scrollbar-thin scrollbar-thumb-neutral-800 scrollbar-track-neutral-900  h-[100vh] overflow-auto pb-12">
+          {isSettingsPage ? (
+            <SidebarSettings workspaceSlug={workspace.slug} />
+          ) : (
+            <SidebarMain workspace={workspace} systems={systems} />
+          )}
+        </div>
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel defaultSize={90}>{children}</ResizablePanel>
