8bb6ac1f3439dacea300a544876761ff327dee73
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-09-25 23:41:06 -0500

    OTEL fix

--- apps/webservice/src/instrumentation.ts
+++ apps/webservice/src/instrumentation.ts
@@ -1,21 +1,5 @@
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
-import { PgInstrumentation } from "@opentelemetry/instrumentation-pg";
-import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
-import { registerOTel } from "@vercel/otel";
-
-export function register() {
-  registerOTel({ serviceName: "ctrlplane/webservice" });
-
-  const provider = new NodeTracerProvider();
-  provider.register();
-
-  registerInstrumentations({
-    instrumentations: [
-      new PgInstrumentation({
-        enhancedDatabaseReporting: true,
-        enabled: true,
-        addSqlCommenterCommentToQueries: true,
-      }),
-    ],
-  });
+export async function register() {
+  // eslint-disable-next-line no-restricted-properties
+  if (process.env.NEXT_RUNTIME === "nodejs")
+    await import("./instrumentation.node");
 }
