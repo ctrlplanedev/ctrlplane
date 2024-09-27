3cc5a9f58b1e16ad1815d6c5fe57fc1b74459b9a
Justin Brooks <jsbroks@gmail.com>
2024-09-27 14:34:33 -0400

    remove migrations

--- packages/db/drizzle/0003_amused_bucky.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "workspace_email_domain_matching" ADD COLUMN "role_id" uuid NOT NULL;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "workspace_email_domain_matching" ADD CONSTRAINT "workspace_email_domain_matching_role_id_role_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."role"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
