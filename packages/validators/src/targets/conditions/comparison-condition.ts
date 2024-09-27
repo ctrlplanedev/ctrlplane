c005341384589d279da104915bdee8cda7b9eaa7
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-09-27 15:29:27 -0700

    fix: Not condition

--- packages/validators/src/targets/conditions/comparison-condition.ts
+++ packages/validators/src/targets/conditions/comparison-condition.ts
@@ -11,6 +11,7 @@ export const comparisonCondition: z.ZodType<ComparisonCondition> = z.lazy(() =>
   z.object({
     type: z.literal("comparison"),
     operator: z.literal("or").or(z.literal("and")),
+    not: z.boolean().optional().default(false),
     conditions: z.array(
       z.union([
         metadataCondition,
@@ -25,6 +26,7 @@ export const comparisonCondition: z.ZodType<ComparisonCondition> = z.lazy(() =>
 export type ComparisonCondition = {
   type: "comparison";
   operator: "and" | "or";
+  not?: boolean;
   conditions: Array<
     ComparisonCondition | MetadataCondition | KindCondition | NameCondition
   >;
