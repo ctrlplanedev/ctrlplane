ccb2bd2d64c99ec9148908f696fffe280802df09
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-09-28 04:02:34 -0500

    migration cascade on delete

--- packages/db/drizzle/0003_outstanding_punisher.sql
+++ packages/db/drizzle/0003_outstanding_punisher.sql
@@ -1,5 +1,7 @@
 ALTER TABLE "system" DROP CONSTRAINT "system_workspace_id_workspace_id_fk";
 --> statement-breakpoint
+ALTER TABLE "workspace_email_domain_matching" ALTER COLUMN "verification_code" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "workspace_email_domain_matching" ALTER COLUMN "verification_email" SET NOT NULL;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "system" ADD CONSTRAINT "system_workspace_id_workspace_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "public"."workspace"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
