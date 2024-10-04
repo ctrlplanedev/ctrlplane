a45d02c630664d356f210d973adcdafece009bf7
Justin Brooks <jsbroks@gmail.com>
2024-10-03 22:28:01 -0400

    hide metadata if nothing is set

--- apps/webservice/src/app/[workspaceSlug]/_components/release-drawer/ReleaseDrawer.tsx
+++ apps/webservice/src/app/[workspaceSlug]/_components/release-drawer/ReleaseDrawer.tsx
@@ -1,25 +1,15 @@
 "use client";
 
-import type { Release, ReleaseDependency } from "@ctrlplane/db/schema";
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import {
-  IconInfoCircle,
-  IconProgress,
-  IconSparkles,
-} from "@tabler/icons-react";
-import { format } from "date-fns";
-import yaml from "js-yaml";
+import { IconInfoCircle, IconProgress } from "@tabler/icons-react";
 
 import { cn } from "@ctrlplane/ui";
 import { Button } from "@ctrlplane/ui/button";
 import { Drawer, DrawerContent, DrawerTitle } from "@ctrlplane/ui/drawer";
-import { Input } from "@ctrlplane/ui/input";
-import { ReservedMetadataKey } from "@ctrlplane/validators/targets";
 
 import { api } from "~/trpc/react";
-import { useMatchSorterWithSearch } from "~/utils/useMatchSorter";
-import { ConfigEditor } from "../ConfigEditor";
+import { OverviewContent } from "./OverviewContent";
 
 const TabButton: React.FC<{
   active: boolean;
@@ -112,157 +102,5 @@ export const ReleaseDrawer: React.FC = () => {
         </div>
       </DrawerContent>
     </Drawer>
-  );
-};
-
-const ReleaseConfigInfo: React.FC<{ config: Record<string, any> }> = ({
-  config,
-}) => {
-  yaml.dump(config);
-  return <ConfigEditor value={yaml.dump(config)} readOnly />;
-};
-
-const OverviewContent: React.FC<{
-  release: Release & {
-    metadata: Record<string, string>;
-    dependencies: ReleaseDependency[];
-  };
-}> = ({ release }) => {
-  const { metadata } = release;
-  const links =
-    metadata[ReservedMetadataKey.Links] != null
-      ? (JSON.parse(metadata[ReservedMetadataKey.Links]) as Record<
-          string,
-          string
-        >)
-      : null;
-
-  return (
-    <div className="space-y-4">
-      <div className="space-y-2">
-        <div className="text-sm">Properties</div>
-        <div className="grid grid-cols-2 gap-4">
-          <div>
-            <table
-              width="100%"
-              className="text-xs"
-              style={{ tableLayout: "fixed" }}
-            >
-              <tbody>
-                {release.name !== release.version && (
-                  <tr>
-                    <td className="p-1 pr-2 text-muted-foreground">Name</td>
-                    <td>{release.name}</td>
-                  </tr>
-                )}
-
-                <tr>
-                  <td className="p-1 pr-2 text-muted-foreground">Version</td>
-                  <td>{release.version}</td>
-                </tr>
-
-                <tr>
-                  <td className="p-1 pr-2 text-muted-foreground">Created At</td>
-                  <td>{format(release.createdAt, "MM/dd/yyyy mm:hh:ss")}</td>
-                </tr>
-
-                <tr>
-                  <td className="p-1 pr-2 align-top text-muted-foreground">
-                    Links
-                  </td>
-                  <td>
-                    {links == null ? (
-                      <span className="cursor-help italic text-gray-500">
-                        Not set
-                      </span>
-                    ) : (
-                      <>
-                        {Object.entries(links).map(([name, url]) => (
-                          <a
-                            key={name}
-                            referrerPolicy="no-referrer"
-                            href={url}
-                            className="inline-block w-full overflow-hidden text-ellipsis text-nowrap text-blue-300 hover:text-blue-400"
-                          >
-                            {name}
-                          </a>
-                        ))}
-                      </>
-                    )}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div>
-            <table
-              width="100%"
-              className="text-xs"
-              style={{ tableLayout: "fixed" }}
-            >
-              <tbody>
-                <tr></tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div className="mb-2 text-sm">Config</div>
-        <div className="text-xs">
-          <ReleaseConfigInfo config={release.config} />
-        </div>
-      </div>
-
-      <div>
-        <div className="mb-2 text-sm">
-          Metadata ({Object.keys(metadata).length})
-        </div>
-        <div className="text-xs">
-          <ReleaseMetadataInfo metadata={metadata} />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const ReleaseMetadataInfo: React.FC<{ metadata: Record<string, string> }> = (
-  props,
-) => {
-  const metadata = Object.entries(props.metadata).sort(([keyA], [keyB]) =>
-    keyA.localeCompare(keyB),
-  );
-  const { search, setSearch, result } = useMatchSorterWithSearch(metadata, {
-    keys: ["0", "1"],
-  });
-  return (
-    <div>
-      <div className="text-xs">
-        <div>
-          <Input
-            className="w-full rounded-b-none text-xs"
-            placeholder="Search ..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </div>
-        <div className="scrollbar-thin scrollbar-thumb-neutral-800 scrollbar-track-neutral-900 max-h-[250px] overflow-auto rounded-b-lg border-x border-b p-1.5">
-          {result.map(([key, value]) => (
-            <div className="text-nowrap font-mono" key={key}>
-              <span>
-                {Object.values(ReservedMetadataKey).includes(
-                  key as ReservedMetadataKey,
-                ) && (
-                  <IconSparkles className="inline-block h-3 w-3 text-yellow-300" />
-                )}{" "}
-              </span>
-              <span className="text-red-400">{key}:</span>
-              <span className="text-green-300"> {value}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
   );
 };
