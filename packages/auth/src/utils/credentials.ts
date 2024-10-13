455d24587b41f467d89a3074a4640ffa881553ee
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-10-13 01:44:54 -0500

    fix backend to return an error

--- packages/auth/src/utils/credentials.ts
+++ packages/auth/src/utils/credentials.ts
@@ -13,9 +13,9 @@ const getUserByEmail = (email: string) =>
 
 export const getUserByCredentials = async (email: string, password: string) => {
   const user = await getUserByEmail(email);
-  if (user == null) return null;
+  if (user == null) return new Error("Invalid credentials");
   const { passwordHash } = user;
-  if (passwordHash == null) return null;
+  if (passwordHash == null) return new Error("Invalid credentials");
   const isPasswordCorrect = compareSync(password, passwordHash);
-  return isPasswordCorrect ? user : null;
+  return isPasswordCorrect ? user : new Error("Invalid credentials");
 };
