391a26288e48c72aa2d4fa0c0056164394e12767
Aditya Choudhari <48932219+adityachoudhari26@users.noreply.github.com>
2024-10-15 11:24:31 -0700

    fix: Init cay (#138)

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,7 +1,7 @@
 import { dashboardRouter } from "./router/dashboard";
 import { deploymentRouter } from "./router/deployment";
 import { environmentRouter } from "./router/environment";
-import { githubRouter } from "./router/github/github";
+import { githubRouter } from "./router/github";
 import { jobRouter } from "./router/job";
 import { releaseRouter } from "./router/release";
 import { runbookRouter } from "./router/runbook";
