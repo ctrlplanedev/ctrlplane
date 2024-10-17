4c7e86238e12a9fb9e55b0792258995e5734b62a
Zachary Blasczyk <77289967+zacharyblasczyk@users.noreply.github.com>
2024-10-17 13:41:36 -0500

    Init Terraform Cloud Scanner and API Key docs (#145)

--- apps/docs/pages/integrations/_meta.ts
+++ apps/docs/pages/integrations/_meta.ts
@@ -2,4 +2,6 @@ export default {
   github: "GitHub",
   "google-cloud": "Google Cloud",
   kubernetes: "Kubernetes",
+  "terraform-cloud": "Terraform Cloud",
+  api: "API",
 };
--- apps/docs/pages/self-hosted/_meta.ts
+++ apps/docs/pages/self-hosted/_meta.ts
@@ -1,5 +1,4 @@
 export default {
-  overview: "Overview",
   requirements: "Requirements",
   methods: "Methods",
   "cloud-providers": "Cloud Providers",
