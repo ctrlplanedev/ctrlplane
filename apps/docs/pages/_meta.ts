24ebb43f6fbefc080bd9dbc9fe139a96ea11a066
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-10-19 13:02:35 -0500

    move auth docs

--- apps/docs/pages/_meta.ts
+++ apps/docs/pages/_meta.ts
@@ -4,7 +4,6 @@ export default {
   "core-concepts": "Core Concepts",
   integrations: "Integrations",
   "self-hosted": "Self-hosted",
-  auth: "Auth",
   "config-files": "Config Files",
   glossary: "Glossary",
   troubleshooting: "Troubleshooting",
