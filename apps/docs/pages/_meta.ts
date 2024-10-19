46e015c516d7a399943b183b2eea9a593dbc98d0
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-10-19 12:22:39 -0500

    Auth Docs

--- apps/docs/pages/_meta.ts
+++ apps/docs/pages/_meta.ts
@@ -4,6 +4,7 @@ export default {
   "core-concepts": "Core Concepts",
   integrations: "Integrations",
   "self-hosted": "Self-hosted",
+  auth: "Auth",
   "config-files": "Config Files",
   glossary: "Glossary",
   troubleshooting: "Troubleshooting",
