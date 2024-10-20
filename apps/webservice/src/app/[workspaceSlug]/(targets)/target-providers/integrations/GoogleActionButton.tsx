35186b457edaf02aba96c3deeab444ef922062b5
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-10-19 19:24:43 -0700

    nit

--- apps/webservice/src/app/[workspaceSlug]/(targets)/target-providers/integrations/GoogleActionButton.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(targets)/target-providers/integrations/GoogleActionButton.tsx
@@ -2,12 +2,11 @@
 
 import type { Workspace } from "@ctrlplane/db/schema";
 import React from "react";
-
-//import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@ctrlplane/ui/button";
 
-// import { api } from "~/trpc/react";
+import { api } from "~/trpc/react";
 import { GoogleDialog } from "./google/GoogleDialog";
 
 type GoogleActionButtonProps = {
@@ -17,40 +16,33 @@ type GoogleActionButtonProps = {
 export const GoogleActionButton: React.FC<GoogleActionButtonProps> = ({
   workspace,
 }) => {
-  // const createServiceAccount =
-  //   api.workspace.integrations.google.createServiceAccount.useMutation();
+  const createServiceAccount =
+    api.workspace.integrations.google.createServiceAccount.useMutation();
 
-  //  const router = useRouter();
+  const router = useRouter();
+
+  if (workspace.googleServiceAccountEmail != null)
+    return (
+      <GoogleDialog workspace={workspace}>
+        <Button variant="outline" size="sm" className="w-full">
+          Configure
+        </Button>
+      </GoogleDialog>
+    );
 
   return (
-    <GoogleDialog workspace={workspace}>
-      <Button variant="outline" size="sm" className="w-full">
-        Configure
-      </Button>
-    </GoogleDialog>
+    <Button
+      variant="outline"
+      size="sm"
+      className="w-full"
+      disabled={createServiceAccount.isPending}
+      onClick={async () =>
+        createServiceAccount
+          .mutateAsync(workspace.id)
+          .then(() => router.refresh())
+      }
+    >
+      Enable
+    </Button>
   );
-  // if (workspace.googleServiceAccountEmail != null)
-  //   return (
-  //     <GoogleDialog workspace={workspace}>
-  //       <Button variant="outline" size="sm" className="w-full">
-  //         Configure
-  //       </Button>
-  //     </GoogleDialog>
-  //   );
-
-  // return (
-  //   <Button
-  //     variant="outline"
-  //     size="sm"
-  //     className="w-full"
-  //     disabled={createServiceAccount.isPending}
-  //     onClick={async () =>
-  //       createServiceAccount
-  //         .mutateAsync(workspace.id)
-  //         .then(() => router.refresh())
-  //     }
-  //   >
-  //     Enable
-  //   </Button>
-  // );
 };
