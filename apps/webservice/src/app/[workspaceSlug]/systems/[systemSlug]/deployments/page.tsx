5f500908340828f67c4526c9b7a8ee9d9bb49c98
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-10-24 18:12:14 -0700

    fix

--- apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/page.tsx
@@ -8,7 +8,7 @@ import DeploymentTable from "./TableDeployments";
 
 export const metadata: Metadata = { title: "Deployments - Systems" };
 
-export default async function DeploymentsPage({
+export default async function SystemDeploymentsPage({
   params,
 }: {
   params: { workspaceSlug: string; systemSlug: string };
@@ -28,9 +28,9 @@ export default async function DeploymentsPage({
       ) : (
         <div className="scrollbar-thin scrollbar-thumb-neutral-800 scrollbar-track-neutral-900 container mx-auto h-[calc(100vh-40px)] overflow-auto p-8">
           <DeploymentTable
-            systemSlug={params.systemSlug}
             deployments={deployments}
             environments={environments}
+            systemSlug={params.systemSlug}
             workspaceSlug={params.workspaceSlug}
           />
         </div>
