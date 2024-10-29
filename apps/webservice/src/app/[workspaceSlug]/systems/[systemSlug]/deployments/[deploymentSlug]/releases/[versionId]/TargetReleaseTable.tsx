13a5969f1c1a637fb583fbec52db890e7f74baa0
Zachary Blasczyk <77289967+zacharyblasczyk@users.noreply.github.com>
2024-10-28 22:50:23 -0500

    add badges and fix loading of release jobs (#182)

--- apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/[deploymentSlug]/releases/[versionId]/TargetReleaseTable.tsx
+++ apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/[deploymentSlug]/releases/[versionId]/TargetReleaseTable.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import type { Environment } from "@ctrlplane/db/schema";
+import type { Environment, Target } from "@ctrlplane/db/schema";
+import type { JobStatus } from "@ctrlplane/validators/jobs";
 import React, { Fragment, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -14,7 +15,9 @@ import { capitalCase } from "change-case";
 import _ from "lodash";
 
 import { cn } from "@ctrlplane/ui";
+import { Badge } from "@ctrlplane/ui/badge";
 import { Button, buttonVariants } from "@ctrlplane/ui/button";
+import { Skeleton } from "@ctrlplane/ui/skeleton";
 import { Table, TableBody, TableCell, TableRow } from "@ctrlplane/ui/table";
 import { ReservedMetadataKey } from "@ctrlplane/validators/conditions";
 
@@ -29,38 +32,53 @@ import { PolicyApprovalRow } from "./PolicyApprovalRow";
 
 type CollapsibleTableRowProps = {
   environment: Environment;
+  environmentCount: number;
   deploymentName: string;
   release: {
     id: string;
     version: string;
     name: string;
   };
+  releaseJobTriggerData: Array<{
+    id: string;
+    environmentId: string;
+    job: {
+      id: string;
+      status: JobStatus;
+      metadata: Array<{ key: string; value: string }>;
+      externalId: string | null;
+    };
+    target: Target;
+    type: string;
+  }>;
 };
 
 const CollapsibleTableRow: React.FC<CollapsibleTableRowProps> = ({
   environment,
+  environmentCount,
   deploymentName,
   release,
+  releaseJobTriggerData,
 }) => {
-  const { filter } = useJobFilter();
-  const { setJobId } = useJobDrawer();
   const pathname = usePathname();
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const releaseJobTriggerQuery = api.job.config.byReleaseId.useQuery(
-    { releaseId: release.id, filter },
-    { refetchInterval: 5_000, placeholderData: (prev) => prev },
-  );
-  const jobs = releaseJobTriggerQuery.data?.filter(
+  const { setJobId } = useJobDrawer();
+  const jobs = releaseJobTriggerData.filter(
     (job) => job.environmentId === environment.id,
   );
+
   const approvalsQ = api.environment.policy.approval.byReleaseId.useQuery({
     releaseId: release.id,
   });
+
   const approvals = approvalsQ.data ?? [];
   const environmentApprovals = approvals.filter(
     (approval) => approval.policyId === environment.policyId,
   );
+
+  const isOpen = jobs.length < 10 && environmentCount < 3;
+  const [isExpanded, setIsExpanded] = useState(isOpen);
+
+  if (jobs.length === 0) return null;
 
   return (
     <Fragment>
@@ -78,6 +96,21 @@ const CollapsibleTableRow: React.FC<CollapsibleTableRowProps> = ({
                 )}
               />
               {environment.name}
+              <div className="flex items-center gap-1.5">
+                {Object.entries(_.groupBy(jobs, (job) => job.job.status)).map(
+                  ([status, groupedJobs]) => (
+                    <Badge
+                      key={status}
+                      variant="outline"
+                      className="rounded-full px-1.5 py-0.5"
+                      title={capitalCase(status).replace("_", " ")}
+                    >
+                      <JobTableStatusIcon status={status as JobStatus} />
+                      <span className="pl-1">{groupedJobs.length}</span>
+                    </Badge>
+                  ),
+                )}
+              </div>
             </div>
             <div className="flex items-center gap-2">
               {environmentApprovals.map((approval) => (
@@ -93,7 +126,7 @@ const CollapsibleTableRow: React.FC<CollapsibleTableRowProps> = ({
       </TableRow>
       {isExpanded && (
         <>
-          {jobs?.map((job, idx) => {
+          {jobs.map((job, idx) => {
             const linksMetadata = job.job.metadata.find(
               (m) => m.key === String(ReservedMetadataKey.Links),
             )?.value;
@@ -112,7 +145,7 @@ const CollapsibleTableRow: React.FC<CollapsibleTableRowProps> = ({
                 )}
                 onClick={() => setJobId(job.job.id)}
               >
-                <TableCell>
+                <TableCell onClick={(e) => e.stopPropagation()}>
                   <Link
                     href={`${pathname}?target_id=${job.target.id}`}
                     className="block w-full hover:text-blue-300"
@@ -138,7 +171,7 @@ const CollapsibleTableRow: React.FC<CollapsibleTableRowProps> = ({
                     </span>
                   )}
                 </TableCell>
-                <TableCell>
+                <TableCell onClick={(e) => e.stopPropagation()}>
                   {links != null && (
                     <div className="flex items-center gap-1">
                       {Object.entries(links).map(([label, url]) => (
@@ -199,6 +232,14 @@ export const TargetReleaseTable: React.FC<TargetReleaseTableProps> = ({
   environments,
 }) => {
   const { filter, setFilter } = useJobFilter();
+  const releaseJobTriggerQuery = api.job.config.byReleaseId.useQuery(
+    { releaseId: release.id, filter },
+    {
+      refetchInterval: 5_000,
+      placeholderData: (prev) => prev,
+      initialData: [],
+    },
+  );
 
   return (
     <>
@@ -212,18 +253,35 @@ export const TargetReleaseTable: React.FC<TargetReleaseTableProps> = ({
           </div>
         </JobConditionDialog>
       </div>
-      <Table className="table-fixed">
-        <TableBody>
-          {environments.map((environment) => (
-            <CollapsibleTableRow
-              key={environment.id}
-              environment={environment}
-              deploymentName={deploymentName}
-              release={release}
+
+      {releaseJobTriggerQuery.data.length == 0 && (
+        <div className="space-y-2 p-4">
+          {_.range(30).map((i) => (
+            <Skeleton
+              key={i}
+              className="h-9 w-full"
+              style={{ opacity: 1 * (1 - i / 10) }}
             />
           ))}
-        </TableBody>
-      </Table>
+        </div>
+      )}
+
+      {releaseJobTriggerQuery.data.length > 0 && (
+        <Table className="table-fixed">
+          <TableBody>
+            {environments.map((environment) => (
+              <CollapsibleTableRow
+                key={environment.id}
+                environment={environment}
+                environmentCount={environments.length}
+                deploymentName={deploymentName}
+                release={release}
+                releaseJobTriggerData={releaseJobTriggerQuery.data}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </>
   );
 };
