6faec4470a2c448b5e308703bb9b4bf147581de0
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-10-30 15:51:35 -0700

    drop 28

--- packages/db/drizzle/0028_peaceful_otto_octavius.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "environment" ADD COLUMN "expires_at" timestamp with time zone DEFAULT NULL;--> statement-breakpoint
-ALTER TABLE "environment" DROP COLUMN IF EXISTS "deleted_at";--> statement-breakpoint
-ALTER TABLE "environment_policy" DROP COLUMN IF EXISTS "ephemeral_duration";
