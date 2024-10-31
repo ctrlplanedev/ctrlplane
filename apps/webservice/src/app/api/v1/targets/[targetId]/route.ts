911ce07bca3deeac9064e924fdefad62e287db38
Justin Brooks <jsbroks@gmail.com>
2024-10-31 01:06:02 -0400

    add target PATCH option to api

--- apps/webservice/src/app/api/v1/targets/[targetId]/route.ts
+++ apps/webservice/src/app/api/v1/targets/[targetId]/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server";
+import _ from "lodash";
+import { z } from "zod";
 
 import { eq } from "@ctrlplane/db";
 import * as schema from "@ctrlplane/db/schema";
+import { upsertTargets } from "@ctrlplane/job-dispatch";
 import { Permission } from "@ctrlplane/validators/auth";
 
 import { authn, authz } from "../../auth";
+import { parseBody } from "../../body-parser";
 import { request } from "../../middleware";
 
 export const GET = request()
@@ -35,4 +39,48 @@ export const GET = request()
       ...target,
       metadata: Object.fromEntries(metadata.map((t) => [t.key, t.value])),
     });
+  });
+
+const patchSchema = z.object({
+  name: z.string().optional().optional(),
+  version: z.string().optional().optional(),
+  kind: z.string().optional().optional(),
+  identifier: z.string().optional().optional(),
+  workspaceId: z.string().optional().optional(),
+  metadata: z.record(z.string()).optional(),
+  variables: z
+    .array(
+      z.object({
+        key: z.string(),
+        value: z.union([z.string(), z.number(), z.boolean()]),
+        sensitive: z.boolean().default(false),
+      }),
+    )
+    .optional(),
+});
+
+export const PATCH = request()
+  .use(authn)
+  .use(
+    authz(({ can, extra }) =>
+      can
+        .perform(Permission.TargetUpdate)
+        .on({ type: "target", id: extra.params.targetId }),
+    ),
+  )
+  .use(parseBody(patchSchema))
+  .handle<
+    { body: z.infer<typeof patchSchema> },
+    { params: { targetId: string } }
+  >(async ({ db, body }, { params }) => {
+    const target = await db.query.target.findFirst({
+      where: eq(schema.target.id, params.targetId),
+    });
+
+    if (target == null)
+      return NextResponse.json({ error: "Target not found" }, { status: 404 });
+
+    const t = await upsertTargets(db, [_.merge(target, body)]);
+
+    return NextResponse.json(t[0]);
   });
