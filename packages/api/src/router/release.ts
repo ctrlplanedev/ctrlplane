990ef48a1bef00e2453117c2f9da1ae97e712e23
Justin Brooks <jsbroks@gmail.com>
2024-11-01 01:33:00 -0400

    refactor release endpoint into seperate files

--- packages/api/src/router/release.ts
+++ packages/api/src/router/release.ts
@@ -1,5 +1,3 @@
-import type { Tx } from "@ctrlplane/db";
-import type { ReleaseJobTrigger } from "@ctrlplane/db/schema";
 import _ from "lodash";
 import { isPresent } from "ts-is-present";
 import { z } from "zod";
@@ -10,7 +8,6 @@ import {
   desc,
   eq,
   inArray,
-  notInArray,
   takeFirst,
   takeFirstOrNull,
 } from "@ctrlplane/db";
@@ -29,7 +26,6 @@ import {
   releaseJobTrigger,
   releaseMatchesCondition,
   releaseMetadata,
-  system,
   target,
 } from "@ctrlplane/db/schema";
 import {
@@ -38,13 +34,14 @@ import {
   createReleaseJobTriggers,
   dispatchReleaseJobTriggers,
   isPassingAllPolicies,
-  isPassingLockingPolicy,
   isPassingReleaseStringCheckPolicy,
 } from "@ctrlplane/job-dispatch";
 import { Permission } from "@ctrlplane/validators/auth";
 import { releaseCondition } from "@ctrlplane/validators/releases";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { releaseDeployRouter } from "./release-deploy";
+import { releaseMetadataKeysRouter } from "./release-metadata-keys";
 
 export const releaseRouter = createTRPCRouter({
   list: protectedProcedure
@@ -187,154 +184,7 @@ export const releaseRouter = createTRPCRouter({
         }),
     ),
 
-  deploy: createTRPCRouter({
-    toEnvironment: protectedProcedure
-      .meta({
-        authorizationCheck: ({ canUser, input }) =>
-          canUser
-            .perform(Permission.DeploymentGet, Permission.ReleaseGet)
-            .on(
-              { type: "release", id: input.releaseId },
-              { type: "environment", id: input.environmentId },
-            ),
-      })
-      .input(
-        z.object({
-          environmentId: z.string(),
-          releaseId: z.string(),
-          isForcedRelease: z.boolean().optional(),
-        }),
-      )
-      .mutation(async ({ ctx, input }) => {
-        const cancelPreviousJobs = async (
-          tx: Tx,
-          releaseJobTriggers: ReleaseJobTrigger[],
-        ) =>
-          tx
-            .select()
-            .from(releaseJobTrigger)
-            .where(
-              and(
-                eq(releaseJobTrigger.releaseId, input.releaseId),
-                eq(releaseJobTrigger.environmentId, input.environmentId),
-                notInArray(
-                  releaseJobTrigger.id,
-                  releaseJobTriggers.map((j) => j.id),
-                ),
-              ),
-            )
-            .then((existingReleaseJobTriggers) =>
-              tx
-                .update(job)
-                .set({ status: "cancelled" })
-                .where(
-                  inArray(
-                    job.id,
-                    existingReleaseJobTriggers.map((t) => t.jobId),
-                  ),
-                )
-                .then(() => {}),
-            );
-
-        const releaseJobTriggers = await createReleaseJobTriggers(
-          ctx.db,
-          "force_deploy",
-        )
-          .causedById(ctx.session.user.id)
-          .environments([input.environmentId])
-          .releases([input.releaseId])
-          .filter(
-            input.isForcedRelease
-              ? (_, releaseJobTriggers) => releaseJobTriggers
-              : isPassingReleaseStringCheckPolicy,
-          )
-          .then(input.isForcedRelease ? cancelPreviousJobs : createJobApprovals)
-          .insert();
-
-        await dispatchReleaseJobTriggers(ctx.db)
-          .releaseTriggers(releaseJobTriggers)
-          .filter(
-            input.isForcedRelease
-              ? isPassingLockingPolicy
-              : isPassingAllPolicies,
-          )
-          .then(cancelOldReleaseJobTriggersOnJobDispatch)
-          .dispatch();
-
-        return releaseJobTriggers;
-      }),
-
-    toTarget: protectedProcedure
-      .meta({
-        authorizationCheck: ({ canUser, input }) =>
-          canUser
-            .perform(Permission.ReleaseGet, Permission.TargetUpdate)
-            .on(
-              { type: "release", id: input.releaseId },
-              { type: "target", id: input.targetId },
-            ),
-      })
-      .input(
-        z.object({
-          targetId: z.string().uuid(),
-          releaseId: z.string().uuid(),
-          environmentId: z.string().uuid(),
-          isForcedRelease: z.boolean().optional(),
-        }),
-      )
-      .mutation(async ({ ctx, input }) => {
-        const t = await ctx.db
-          .select()
-          .from(target)
-          .where(eq(target.id, input.targetId))
-          .then(takeFirstOrNull);
-        if (!t) throw new Error("Target not found");
-
-        if (t.lockedAt != null) throw new Error("Target is locked");
-
-        const rel = await ctx.db
-          .select()
-          .from(release)
-          .where(eq(release.id, input.releaseId))
-          .then(takeFirstOrNull);
-        if (!rel) throw new Error("Release not found");
-
-        const env = await ctx.db
-          .select()
-          .from(environment)
-          .where(eq(environment.id, input.environmentId))
-          .then(takeFirstOrNull);
-        if (!env) throw new Error("Environment not found");
-
-        const releaseJobTriggers = await createReleaseJobTriggers(
-          ctx.db,
-          "force_deploy",
-        )
-          .causedById(ctx.session.user.id)
-          .environments([env.id])
-          .releases([rel.id])
-          .targets([t.id])
-          .filter(
-            input.isForcedRelease
-              ? (_, releaseJobTriggers) => releaseJobTriggers
-              : isPassingReleaseStringCheckPolicy,
-          )
-          .then(input.isForcedRelease ? () => {} : createJobApprovals)
-          .insert();
-
-        await dispatchReleaseJobTriggers(ctx.db)
-          .releaseTriggers(releaseJobTriggers)
-          .filter(
-            input.isForcedRelease
-              ? isPassingLockingPolicy
-              : isPassingAllPolicies,
-          )
-          .then(cancelOldReleaseJobTriggersOnJobDispatch)
-          .dispatch();
-
-        return releaseJobTriggers[0]!;
-      }),
-  }),
+  deploy: releaseDeployRouter,
 
   create: protectedProcedure
     .meta({
@@ -503,44 +353,5 @@ export const releaseRouter = createTRPCRouter({
       return Promise.all(blockedEnvsPromises).then((r) => r.filter(isPresent));
     }),
 
-  metadataKeys: createTRPCRouter({
-    bySystem: protectedProcedure
-      .meta({
-        authorizationCheck: ({ canUser, input }) =>
-          canUser.perform(Permission.ReleaseGet).on({
-            type: "system",
-            id: input,
-          }),
-      })
-      .input(z.string().uuid())
-      .query(async ({ input, ctx }) =>
-        ctx.db
-          .selectDistinct({ key: releaseMetadata.key })
-          .from(release)
-          .innerJoin(releaseMetadata, eq(releaseMetadata.releaseId, release.id))
-          .innerJoin(deployment, eq(release.deploymentId, deployment.id))
-          .where(eq(deployment.systemId, input))
-          .then((r) => r.map((row) => row.key)),
-      ),
-
-    byWorkspace: protectedProcedure
-      .meta({
-        authorizationCheck: ({ canUser, input }) =>
-          canUser.perform(Permission.ReleaseGet).on({
-            type: "workspace",
-            id: input,
-          }),
-      })
-      .input(z.string().uuid())
-      .query(async ({ input, ctx }) =>
-        ctx.db
-          .selectDistinct({ key: releaseMetadata.key })
-          .from(release)
-          .innerJoin(releaseMetadata, eq(releaseMetadata.releaseId, release.id))
-          .innerJoin(deployment, eq(release.deploymentId, deployment.id))
-          .innerJoin(system, eq(deployment.systemId, system.id))
-          .where(eq(system.workspaceId, input))
-          .then((r) => r.map((row) => row.key)),
-      ),
-  }),
+  metadataKeys: releaseMetadataKeysRouter,
 });
