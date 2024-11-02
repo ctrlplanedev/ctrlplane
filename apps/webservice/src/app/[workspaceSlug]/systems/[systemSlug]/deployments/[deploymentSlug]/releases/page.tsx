ac397cc00633e33c21f3628a6fd9c5362a01bc5f
Justin Brooks <jsbroks@gmail.com>
2024-11-02 00:51:45 -0400

    add more metadata to tab

--- apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/[deploymentSlug]/releases/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/systems/[systemSlug]/deployments/[deploymentSlug]/releases/page.tsx
@@ -1,15 +1,27 @@
 "use server";
 
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import { api } from "~/trpc/server";
 import { DeploymentPageContent } from "./DeploymentPageContent";
 
-export default async function DeploymentPage({
-  params,
-}: {
+type PageProps = {
   params: { workspaceSlug: string; systemSlug: string; deploymentSlug: string };
-}) {
+};
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const deployment = await api.deployment.bySlug(params);
+  if (deployment == null) return notFound();
+
+  return {
+    title: `Releases | ${deployment.name}`,
+  };
+}
+
+export default async function DeploymentPage({ params }: PageProps) {
   const deployment = await api.deployment.bySlug(params);
   if (deployment == null) return notFound();
   const { system } = deployment;
