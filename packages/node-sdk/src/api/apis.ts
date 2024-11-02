191be9910ce96132db9f6d1c18ef0f5e624a4118
Justin Brooks <jsbroks@gmail.com>
2024-11-01 22:32:10 -0400

    refactor api

---
+++ packages/node-sdk/src/api/apis.ts
@@ -0,0 +1,20 @@
+import * as http from "http";
+
+import { DefaultApi } from "./defaultApi";
+
+export * from "./defaultApi";
+
+export class HttpError extends Error {
+  constructor(
+    public response: http.IncomingMessage,
+    public body: any,
+    public statusCode?: number,
+  ) {
+    super("HTTP request failed");
+    this.name = "HttpError";
+  }
+}
+
+export { RequestFile } from "../model/models";
+
+export const APIS = [DefaultApi];
--- packages/node-sdk/src/index.ts
+++ packages/node-sdk/src/index.ts
@@ -1,5 +1,1 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./runtime";
-export * from "./apis/index";
-export * from "./models/index";
+export * from "./api";
