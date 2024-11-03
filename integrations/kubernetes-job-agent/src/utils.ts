c6cf74155bdd216a60b525935976908062b2606f
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-11-03 00:29:04 -0500

    fix

--- integrations/kubernetes-job-agent/src/utils.ts
+++ integrations/kubernetes-job-agent/src/utils.ts
@@ -1,7 +1,7 @@
 import type { Operations } from "@ctrlplane/node-sdk";
 
-export function omitNullUndefined(obj: Record<string, any>) {
-  return Object.entries(obj).reduce<Record<string, any>>(
+export function omitNullUndefined(obj: object) {
+  return Object.entries(obj).reduce<Record<string, string>>(
     (acc, [key, value]) => {
       if (value !== null && value !== undefined) acc[key] = value;
       return acc;
