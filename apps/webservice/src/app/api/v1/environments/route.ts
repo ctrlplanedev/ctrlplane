b2afd96e9090b5efb33d6a3bf844622d358b7a68
Aditya Choudhari <48932219+adityachoudhari26@users.noreply.github.com>
2024-11-02 21:14:47 -0700

    fix: Create jobs for latest valid releases on env create (#197)

--- apps/webservice/src/app/api/v1/environments/route.ts
+++ apps/webservice/src/app/api/v1/environments/route.ts
@@ -1,4 +1,5 @@
 import type { PermissionChecker } from "@ctrlplane/auth/utils";
+import type { Tx } from "@ctrlplane/db";
 import type { User } from "@ctrlplane/db/schema";
 import { NextResponse } from "next/server";
 import { isPresent } from "ts-is-present";
@@ -6,6 +7,7 @@ import { z } from "zod";
 
 import { takeFirst } from "@ctrlplane/db";
 import * as schema from "@ctrlplane/db/schema";
+import { createJobsForNewEnvironment } from "@ctrlplane/job-dispatch";
 import { Permission } from "@ctrlplane/validators/auth";
 
 import { authn, authz } from "../auth";
@@ -18,6 +20,19 @@ const body = schema.createEnvironment.extend({
     .min(new Date(), "Expires at must be in the future")
     .optional(),
 });
+
+const createReleaseChannels = (
+  db: Tx,
+  environmentId: string,
+  releaseChannels: { channelId: string; deploymentId: string }[],
+) =>
+  db.insert(schema.environmentReleaseChannel).values(
+    releaseChannels.map(({ channelId, deploymentId }) => ({
+      environmentId,
+      channelId,
+      deploymentId,
+    })),
+  );
 
 export const POST = request()
   .use(authn)
@@ -41,7 +56,19 @@ export const POST = request()
         })
         .returning()
         .then(takeFirst)
-        .then((environment) => NextResponse.json({ environment }))
+        .then(async (environment) => {
+          if (
+            isPresent(ctx.body.releaseChannels) &&
+            ctx.body.releaseChannels.length > 0
+          )
+            await createReleaseChannels(
+              ctx.db,
+              environment.id,
+              ctx.body.releaseChannels,
+            );
+          await createJobsForNewEnvironment(ctx.db, environment);
+          return NextResponse.json({ environment });
+        })
         .catch(() =>
           NextResponse.json(
             { error: "Failed to create environment" },
