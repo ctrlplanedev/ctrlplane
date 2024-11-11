7d1136b2c49dbee7cf40f60b55be2538bfb7c10c
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-11-11 12:04:14 -0800

    changes

--- packages/job-dispatch/src/pending-job-checker.ts
+++ packages/job-dispatch/src/pending-job-checker.ts
@@ -1,4 +1,4 @@
-import { and, eq, or } from "@ctrlplane/db";
+import { and, eq, inArray, or } from "@ctrlplane/db";
 import * as SCHEMA from "@ctrlplane/db/schema";
 import { JobStatus } from "@ctrlplane/validators/jobs";
 
@@ -8,40 +8,47 @@ export const isPassingNoPendingJobsPolicy: ReleasePolicyChecker = async (
   db,
   wf,
 ) =>
-  db
-    .selectDistinctOn([
-      SCHEMA.releaseJobTrigger.releaseId,
-      SCHEMA.releaseJobTrigger.targetId,
-      SCHEMA.releaseJobTrigger.environmentId,
-    ])
-    .from(SCHEMA.job)
-    .innerJoin(
-      SCHEMA.releaseJobTrigger,
-      eq(SCHEMA.releaseJobTrigger.jobId, SCHEMA.job.id),
-    )
-    .where(
-      or(
-        ...wf.map((w) =>
+  wf.length > 0
+    ? db
+        .selectDistinctOn([
+          SCHEMA.releaseJobTrigger.releaseId,
+          SCHEMA.releaseJobTrigger.targetId,
+          SCHEMA.releaseJobTrigger.environmentId,
+        ])
+        .from(SCHEMA.job)
+        .innerJoin(
+          SCHEMA.releaseJobTrigger,
+          eq(SCHEMA.releaseJobTrigger.jobId, SCHEMA.job.id),
+        )
+        .where(
           and(
-            eq(SCHEMA.releaseJobTrigger.environmentId, w.environmentId),
-            eq(SCHEMA.releaseJobTrigger.releaseId, w.releaseId),
-            eq(SCHEMA.releaseJobTrigger.targetId, w.targetId),
+            inArray(
+              SCHEMA.releaseJobTrigger.environmentId,
+              wf.map((w) => w.environmentId),
+            ),
+            inArray(
+              SCHEMA.releaseJobTrigger.releaseId,
+              wf.map((w) => w.releaseId),
+            ),
+            inArray(
+              SCHEMA.releaseJobTrigger.targetId,
+              wf.map((w) => w.targetId),
+            ),
             or(
               eq(SCHEMA.job.status, JobStatus.Pending),
               eq(SCHEMA.job.status, JobStatus.InProgress),
             ),
           ),
-        ),
-      ),
-    )
-    .then((rows) =>
-      wf.filter(
-        (w) =>
-          !rows.some(
-            (r) =>
-              r.release_job_trigger.releaseId === w.releaseId &&
-              r.release_job_trigger.targetId === w.targetId &&
-              r.release_job_trigger.environmentId === w.environmentId,
+        )
+        .then((rows) =>
+          wf.filter(
+            (w) =>
+              !rows.some(
+                (r) =>
+                  r.release_job_trigger.releaseId === w.releaseId &&
+                  r.release_job_trigger.targetId === w.targetId &&
+                  r.release_job_trigger.environmentId === w.environmentId,
+              ),
           ),
-      ),
-    );
+        )
+    : [];
