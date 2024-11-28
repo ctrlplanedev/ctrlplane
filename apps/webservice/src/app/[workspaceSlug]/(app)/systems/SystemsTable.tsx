c1da10d5a6e600c9b231ab7eaa298788ceee9cb6
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-11-27 19:58:57 -0800

    fix: Clean up system filters

--- apps/webservice/src/app/[workspaceSlug]/(app)/systems/SystemsTable.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(app)/systems/SystemsTable.tsx
@@ -1,92 +1,45 @@
 "use client";
 
 import type { Environment, System } from "@ctrlplane/db/schema";
-import type { ColumnDef } from "@tanstack/react-table";
-import { Fragment } from "react";
 import { useRouter } from "next/navigation";
 import { IconDotsVertical } from "@tabler/icons-react";
-import {
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
 
 import { Button } from "@ctrlplane/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@ctrlplane/ui/table";
+import { Table, TableBody, TableCell, TableRow } from "@ctrlplane/ui/table";
 
 import { SystemActionsDropdown } from "./SystemActionsDropdown";
-
-const columns: ColumnDef<System & { environments: Environment[] }>[] = [
-  {
-    id: "name",
-    header: "Name",
-    accessorKey: "name",
-    cell: (info) => info.getValue(),
-  },
-];
 
 export const SystemsTable: React.FC<{
   systems: (System & { environments: Environment[] })[];
   workspaceSlug: string;
 }> = ({ systems, workspaceSlug }) => {
   const router = useRouter();
-  const table = useReactTable({
-    data: systems,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-
   return (
-    <Table>
-      <TableHeader>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <TableHead key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-              </TableHead>
-            ))}
-          </TableRow>
-        ))}
-      </TableHeader>
+    <Table className="table-fixed">
       <TableBody>
-        {table.getRowModel().rows.map((row) => (
+        {systems.map((system) => (
           <TableRow
-            className="flex cursor-pointer items-center justify-between border-b-neutral-800/50 hover:bg-inherit"
-            key={row.id}
+            className="cursor-pointer border-b-neutral-800/50"
+            key={system.id}
           >
-            {row.getVisibleCells().map((cell) => (
-              <Fragment key={cell.id}>
-                <TableCell
-                  onClick={() =>
-                    router.push(
-                      `/${workspaceSlug}/systems/${row.original.slug}/deployments`,
-                    )
-                  }
-                >
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-                <TableCell>
-                  <SystemActionsDropdown system={cell.row.original}>
-                    <Button variant="ghost" size="icon" className="h-6 w-6">
-                      <IconDotsVertical className="h-3 w-3" />
-                    </Button>
-                  </SystemActionsDropdown>
-                </TableCell>
-              </Fragment>
-            ))}
+            <TableCell
+              onClick={() =>
+                router.push(
+                  `/${workspaceSlug}/systems/${system.slug}/deployments`,
+                )
+              }
+            >
+              {system.name}
+            </TableCell>
+            <TableCell>
+              <div className="flex justify-end">
+                <SystemActionsDropdown system={system}>
+                  <Button variant="ghost" size="icon" className="h-6 w-6">
+                    <IconDotsVertical className="h-3 w-3" />
+                  </Button>
+                </SystemActionsDropdown>
+              </div>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
