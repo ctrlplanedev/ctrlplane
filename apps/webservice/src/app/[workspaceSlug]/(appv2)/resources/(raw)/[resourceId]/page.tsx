455f03b733c5c41e645b4f869466305ad5e4c197
Aditya Choudhari <48932219+adityachoudhari26@users.noreply.github.com>
2025-02-11 10:00:08 -0700

    fix: Add deployments table (#305)
    Co-authored-by: Justin Brooks <justin@wandb.com>

--- apps/webservice/src/app/[workspaceSlug]/(appv2)/resources/(raw)/[resourceId]/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(appv2)/resources/(raw)/[resourceId]/page.tsx
@@ -1,3 +1,16 @@
-export default function ResourcePage() {
-  return <div>Hello</div>;
+import { notFound } from "next/navigation";
+
+import { api } from "~/trpc/server";
+import { ResourceDeploymentsTable } from "./deployments/ResourceDeploymentTable";
+
+type Params = Promise<{ resourceId: string }>;
+
+export default async function ResourcePage(props: { params: Params }) {
+  const params = await props.params;
+  const { resourceId } = params;
+
+  const resource = await api.resource.byId(resourceId);
+  if (resource == null) notFound();
+
+  return <ResourceDeploymentsTable resource={resource} />;
 }
