0cc1d83f066b8e2e36525cf884319782945d2d8f
Justin Brooks <justin@wandb.com>
2025-02-12 00:14:46 -0500

    add variables page

--- apps/webservice/src/app/[workspaceSlug]/(appv2)/systems/[systemSlug]/(sidebar)/variables/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(appv2)/systems/[systemSlug]/(sidebar)/variables/page.tsx
@@ -1,15 +1,32 @@
+import { notFound } from "next/navigation";
+
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbList,
   BreadcrumbPage,
 } from "@ctrlplane/ui/breadcrumb";
+import { Button } from "@ctrlplane/ui/button";
 import { Separator } from "@ctrlplane/ui/separator";
 import { SidebarTrigger } from "@ctrlplane/ui/sidebar";
 
 import { PageHeader } from "~/app/[workspaceSlug]/(appv2)/_components/PageHeader";
+import { api } from "~/trpc/server";
+import { CreateVariableSetDialog } from "./CreateVariableSetDialog";
+import { VariableSetGettingStarted } from "./GettingStartedVariableSets";
+import { VariableSetsTable } from "./VariableSetsTable";
 
-export default function VariablesPage() {
+export default async function SystemVariableSetsPage(props: {
+  params: Promise<{ workspaceSlug: string; systemSlug: string }>;
+}) {
+  const params = await props.params;
+  const workspace = await api.workspace
+    .bySlug(params.workspaceSlug)
+    .catch(() => notFound());
+  if (!workspace) notFound();
+  const system = await api.system.bySlug(params).catch(() => notFound());
+
+  const variableSets = await api.variableSet.bySystemId(system.id);
   return (
     <div>
       <PageHeader>
@@ -22,7 +39,29 @@ export default function VariablesPage() {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
+
+        <div className="flex-grow" />
+        <CreateVariableSetDialog
+          systemId={system.id}
+          environments={system.environments}
+        >
+          <Button size="sm" variant="outline">
+            Create Variable Set
+          </Button>
+        </CreateVariableSetDialog>
       </PageHeader>
+
+      {variableSets.length === 0 && (
+        <VariableSetGettingStarted
+          systemId={system.id}
+          environments={system.environments}
+        />
+      )}
+      {variableSets.length !== 0 && (
+        <div className="scrollbar-thin scrollbar-thumb-neutral-700 scrollbar-track-neutral-800 h-[calc(100vh-110px)] overflow-auto">
+          <VariableSetsTable variableSets={variableSets} />
+        </div>
+      )}
     </div>
   );
 }
