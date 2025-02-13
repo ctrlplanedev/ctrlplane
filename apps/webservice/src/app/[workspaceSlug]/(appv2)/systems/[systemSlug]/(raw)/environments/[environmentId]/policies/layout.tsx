b68469bbea83196b734d8d5e0f6cf0196043ab30
Aditya Choudhari <aditya.choudhari@wandb.com>
2025-02-13 14:37:38 -0800

    use floating tabs

--- apps/webservice/src/app/[workspaceSlug]/(appv2)/systems/[systemSlug]/(raw)/environments/[environmentId]/policies/layout.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(appv2)/systems/[systemSlug]/(raw)/environments/[environmentId]/policies/layout.tsx
@@ -1,56 +1,22 @@
 import { notFound } from "next/navigation";
 
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarInset,
-  SidebarMenu,
-  SidebarProvider,
-} from "@ctrlplane/ui/sidebar";
-
-import { SidebarLink } from "~/app/[workspaceSlug]/(appv2)/resources/(sidebar)/SidebarLink";
 import { api } from "~/trpc/server";
+import { PolicyTabs } from "./PolicyTabs";
 
 export default async function PoliciesLayout(props: {
   children: React.ReactNode;
-  params: Promise<{
-    workspaceSlug: string;
-    systemSlug: string;
-    environmentId: string;
-  }>;
+  params: Promise<{ environmentId: string }>;
 }) {
-  const params = await props.params;
-  const environment = await api.environment.byId(params.environmentId);
+  const { environmentId } = await props.params;
+
+  const environment = await api.environment.byId(environmentId);
   if (environment == null) notFound();
 
-  const url = (tab: string) =>
-    `/${params.workspaceSlug}/systems/${params.systemSlug}/environments/${params.environmentId}/policies/${tab}`;
-
   return (
-    <SidebarProvider className="relative">
-      <Sidebar className="absolute bottom-0 left-0">
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarLink href={url("approval")}>
-                Approval & Governance
-              </SidebarLink>
-              <SidebarLink href={url("control")}>
-                Deployment Control
-              </SidebarLink>
-              <SidebarLink href={url("management")}>
-                Release Management
-              </SidebarLink>
-              <SidebarLink href={url("channels")}>Release Channels</SidebarLink>
-              <SidebarLink href={url("rollout")}>Rollout & Timing</SidebarLink>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
-      <SidebarInset className="container h-[calc(100vh-56px-64px-2px)] pt-8">
-        {props.children}
-      </SidebarInset>
-    </SidebarProvider>
+    <div className="container flex h-full flex-col gap-4 p-4">
+      <h1 className="text-2xl font-bold">{`${environment.name}'s policies`}</h1>
+      <PolicyTabs />
+      <div className="h-full p-4">{props.children}</div>
+    </div>
   );
 }
