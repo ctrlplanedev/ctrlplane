228352d11d9751b83612f9fae816b22f686dd821
Aditya Choudhari <aditya.choudhari@wandb.com>
2025-02-19 14:09:06 -0800

    fix: Lint

--- packages/ui/src/sidebar.tsx
+++ packages/ui/src/sidebar.tsx
@@ -128,7 +128,7 @@ const SidebarProvider = React.forwardRef<
 
       window.addEventListener("keydown", handleKeyDown);
       return () => window.removeEventListener("keydown", handleKeyDown);
-    }, [toggleSidebar]);
+    }, [toggleSidebar, sidebarNames]);
 
     // We add a state so that we can do data-state="expanded" or "collapsed".
     // This makes it easier to style the sidebar with Tailwind classes.
