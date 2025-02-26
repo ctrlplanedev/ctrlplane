4482389b1584279e82890e33263e68eab45653e0
Justin Brooks <justin@wandb.com>
2025-02-25 21:24:00 -0500

    add create system dialog

--- apps/webservice/src/app/[workspaceSlug]/(appv2)/systems/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(appv2)/systems/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@ctrlplane/ui/button";
 
 import { api } from "~/trpc/server";
 import { CreateDeploymentDialog } from "../_components/deployments/CreateDeployment";
+import { CreateSystemDialog } from "../../(app)/_components/CreateSystem";
 import { SystemDeploymentTable } from "./[systemSlug]/system-deployment-table/SystemDeploymentTable";
 
 export default async function SystemsPage(props: {
@@ -21,9 +22,14 @@ export default async function SystemsPage(props: {
     <div className="container m-8 mx-auto space-y-8">
       <div className="flex w-full items-center justify-between">
         <h2 className="text-2xl font-bold">Systems</h2>
-        <CreateDeploymentDialog>
-          <Button variant="outline">Create Deployment</Button>
-        </CreateDeploymentDialog>
+        <div className="flex items-center gap-2">
+          <CreateSystemDialog workspace={workspace}>
+            <Button variant="outline">New System</Button>
+          </CreateSystemDialog>
+          <CreateDeploymentDialog>
+            <Button variant="outline">New Deployment</Button>
+          </CreateDeploymentDialog>
+        </div>
       </div>
 
       {systems.items.map((s) => (
