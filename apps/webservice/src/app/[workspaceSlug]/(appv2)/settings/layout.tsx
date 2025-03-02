947211579e8413f27237e2b21cb0e626fe91ba16
Aditya Choudhari <aditya.choudhari@wandb.com>
2025-03-02 08:59:57 -0800

    fix: Remove unecessary scrolling on workspace page

--- apps/webservice/src/app/[workspaceSlug]/(appv2)/settings/layout.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(appv2)/settings/layout.tsx
@@ -83,7 +83,7 @@ export default async function Layout(props: {
             </SidebarGroup>
           </SidebarContent>
         </Sidebar>
-        <SidebarInset className="h-[calc(100vh-56px-1px)]">
+        <SidebarInset className="h-[calc(100vh-56px-2px)]">
           {props.children}
         </SidebarInset>
       </SidebarProvider>
