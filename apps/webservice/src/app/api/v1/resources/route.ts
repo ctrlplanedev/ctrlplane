3e906692fcbadb0045f9f34799976bcb60ff61dc
jonathan meeks <jonathanmeeks@gmail.com>
2025-03-12 15:17:19 -0700

    list apis for resources, deployments, systems, env

--- apps/webservice/src/app/api/v1/resources/route.ts
+++ apps/webservice/src/app/api/v1/resources/route.ts
@@ -1,15 +1,19 @@
 import type * as schema from "@ctrlplane/db/schema";
 import { NextResponse } from "next/server";
 import { z } from "zod";
+import httpStatus from "http-status";
 
 import { db } from "@ctrlplane/db/client";
-import { createResource } from "@ctrlplane/db/schema";
+import {createResource, resource} from "@ctrlplane/db/schema";
 import { upsertResources } from "@ctrlplane/job-dispatch";
 import { Permission } from "@ctrlplane/validators/auth";
 
 import { authn, authz } from "../auth";
 import { parseBody } from "../body-parser";
 import { request } from "../middleware";
+import {logger} from "@ctrlplane/logger";
+
+const log = logger.child({ module: "api/v1/deployments" });
 
 const patchBodySchema = z.object({
   workspaceId: z.string().uuid(),
@@ -66,3 +70,36 @@ export const POST = request()
       return NextResponse.json({ count: resources.all.length });
     },
   );
+
+export const GET = request()
+    .use(authn)
+    .use(
+        authz(({ ctx, can }) =>
+            can
+                .perform(Permission.ResourceList)
+                .on({ type: "workspace", id: ctx.body.workspaceId }),
+        ),
+    )
+    .handle(async (ctx) =>
+        ctx.db
+            .select()
+            .from(resource)
+            .orderBy(resource.name)
+            .then((environments) => ({ data: environments }))
+            .then((paginated) =>
+                NextResponse.json(paginated, { status: httpStatus.CREATED }),
+            )
+            .catch((error) => {
+                if (error instanceof z.ZodError)
+                    return NextResponse.json(
+                        { error: error.errors },
+                        { status: httpStatus.BAD_REQUEST },
+                    );
+
+                log.error("Error getting systems:", error);
+                return NextResponse.json(
+                    { error: "Internal Server Error" },
+                    { status: httpStatus.INTERNAL_SERVER_ERROR },
+                );
+            }),
+    );
