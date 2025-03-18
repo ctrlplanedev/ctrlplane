8cc3d1a115be5cf1415ebc2bdadddbfb4c730e2e
Aditya Choudhari <aditya.choudhari@wandb.com>
2025-03-18 14:20:56 -0700

    cardify

--- apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/deployments/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/deployments/page.tsx
@@ -1,8 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@ctrlplane/ui/card";
+
 import { EnvironmentDeploymentsPageContent } from "./EnvironmentDeploymentsPageContent";
 
 export default async function DeploymentsPage(props: {
   params: Promise<{ environmentId: string }>;
 }) {
   const { environmentId } = await props.params;
-  return <EnvironmentDeploymentsPageContent environmentId={environmentId} />;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Deployments</CardTitle>
+        <CardDescription>View detailed deployment information</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <EnvironmentDeploymentsPageContent environmentId={environmentId} />
+      </CardContent>
+    </Card>
+  );
 }
--- apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/resources/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/resources/page.tsx
@@ -1,8 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@ctrlplane/ui/card";
+
 import { ResourcesPageContent } from "./ResourcesPageContent";
 
 export default async function ResourcesPage(props: {
   params: Promise<{ environmentId: string }>;
 }) {
   const { environmentId } = await props.params;
-  return <ResourcesPageContent environmentId={environmentId} />;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Resources</CardTitle>
+        <CardDescription>Resources managed in this environment</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ResourcesPageContent environmentId={environmentId} />
+      </CardContent>
+    </Card>
+  );
 }
