78fbfbb00c42792e976f2e9cf82833dfc9bb9b15
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2025-03-22 22:00:32 -0500

    fix: bypass

--- packages/job-dispatch/src/pending-job-checker.ts
+++ packages/job-dispatch/src/pending-job-checker.ts
@@ -4,51 +4,70 @@ import { JobStatus } from "@ctrlplane/validators/jobs";
 
 import type { ReleasePolicyChecker } from "./policies/utils";
 
+const shouldBypassCheck = (trigger: {
+  type: (typeof SCHEMA.releaseJobTriggerType.enumValues)[number];
+  resourceChanged?: boolean;
+}) => trigger.type === "variable_changed";
+
 export const isPassingNoPendingJobsPolicy: ReleasePolicyChecker = async (
   db,
   wf,
 ) =>
-  wf.length > 0
-    ? db
-        .selectDistinctOn([
-          SCHEMA.releaseJobTrigger.versionId,
-          SCHEMA.releaseJobTrigger.resourceId,
-          SCHEMA.releaseJobTrigger.environmentId,
-        ])
-        .from(SCHEMA.job)
-        .innerJoin(
-          SCHEMA.releaseJobTrigger,
-          eq(SCHEMA.releaseJobTrigger.jobId, SCHEMA.job.id),
-        )
-        .where(
-          and(
-            inArray(
-              SCHEMA.releaseJobTrigger.environmentId,
-              wf.map((w) => w.environmentId),
-            ),
-            inArray(
-              SCHEMA.releaseJobTrigger.versionId,
-              wf.map((w) => w.versionId),
-            ),
-            inArray(
-              SCHEMA.releaseJobTrigger.resourceId,
-              wf.map((w) => w.resourceId),
-            ),
-            or(
-              eq(SCHEMA.job.status, JobStatus.Pending),
-              eq(SCHEMA.job.status, JobStatus.InProgress),
-            ),
-          ),
-        )
-        .then((rows) =>
-          wf.filter(
-            (w) =>
-              !rows.some(
-                (r) =>
-                  r.release_job_trigger.versionId === w.versionId &&
-                  r.release_job_trigger.resourceId === w.resourceId &&
-                  r.release_job_trigger.environmentId === w.environmentId,
-              ),
-          ),
-        )
-    : [];
+  wf.length === 0
+    ? []
+    : [
+        ...wf.filter(shouldBypassCheck),
+        ...(await (wf.some((t) => !shouldBypassCheck(t))
+          ? db
+              .selectDistinctOn([
+                SCHEMA.releaseJobTrigger.versionId,
+                SCHEMA.releaseJobTrigger.resourceId,
+                SCHEMA.releaseJobTrigger.environmentId,
+              ])
+              .from(SCHEMA.job)
+              .innerJoin(
+                SCHEMA.releaseJobTrigger,
+                eq(SCHEMA.releaseJobTrigger.jobId, SCHEMA.job.id),
+              )
+              .where(
+                and(
+                  inArray(
+                    SCHEMA.releaseJobTrigger.environmentId,
+                    wf
+                      .filter((t) => !shouldBypassCheck(t))
+                      .map((t) => t.environmentId),
+                  ),
+                  inArray(
+                    SCHEMA.releaseJobTrigger.versionId,
+                    wf
+                      .filter((t) => !shouldBypassCheck(t))
+                      .map((t) => t.versionId),
+                  ),
+                  inArray(
+                    SCHEMA.releaseJobTrigger.resourceId,
+                    wf
+                      .filter((t) => !shouldBypassCheck(t))
+                      .map((t) => t.resourceId),
+                  ),
+                  or(
+                    eq(SCHEMA.job.status, JobStatus.Pending),
+                    eq(SCHEMA.job.status, JobStatus.InProgress),
+                  ),
+                ),
+              )
+              .then((rows) =>
+                wf
+                  .filter((t) => !shouldBypassCheck(t))
+                  .filter(
+                    (t) =>
+                      !rows.some(
+                        (r) =>
+                          r.release_job_trigger.versionId === t.versionId &&
+                          r.release_job_trigger.resourceId === t.resourceId &&
+                          r.release_job_trigger.environmentId ===
+                            t.environmentId,
+                      ),
+                  ),
+              )
+          : [])),
+      ];
