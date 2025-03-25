d7ae1afb3a5c5a5034eeac62d1030f29e9665ae8
Aditya Choudhari <aditya.choudhari@wandb.com>
2025-03-24 23:17:27 -0700

    fix: Resource filter is configurable

---
+++ apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/resources/_components/EditSelector.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import type * as SCHEMA from "@ctrlplane/db/schema";
+import type { ResourceCondition } from "@ctrlplane/validators/resources";
+import type React from "react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@ctrlplane/ui/button";
+
+import { ResourceConditionDialog } from "~/app/[workspaceSlug]/(app)/_components/resources/condition/ResourceConditionDialog";
+import { api } from "~/trpc/react";
+
+export const EditSelector: React.FC<{
+  environment: SCHEMA.Environment;
+}> = ({ environment }) => {
+  const updateEnvironment = api.environment.update.useMutation();
+  const router = useRouter();
+
+  const onChange = (resourceCondition: ResourceCondition | null) =>
+    updateEnvironment
+      .mutateAsync({
+        id: environment.id,
+        data: { resourceFilter: resourceCondition ?? undefined },
+      })
+      .then(() => router.refresh());
+
+  return (
+    <ResourceConditionDialog
+      condition={environment.resourceFilter}
+      onChange={onChange}
+    >
+      <Button size="sm" variant="outline">
+        Edit Selector
+      </Button>
+    </ResourceConditionDialog>
+  );
+};
--- apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/resources/page.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/resources/page.tsx
@@ -1,13 +1,5 @@
 import { notFound } from "next/navigation";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@ctrlplane/ui/card";
-
 import { api } from "~/trpc/server";
 import { ResourcesPageContent } from "./ResourcesPageContent";
 
@@ -20,34 +12,10 @@ export default async function ResourcesPage(props: {
   const environment = await api.environment.byId(environmentId);
   if (environment == null) return notFound();
 
-  const { resourceFilter } = environment;
-  if (resourceFilter == null)
-    return (
-      <Card>
-        <CardHeader>
-          <CardTitle>Resources</CardTitle>
-          <CardDescription>
-            Resources managed in this environment
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>No resource filter set for this environment</p>
-        </CardContent>
-      </Card>
-    );
-
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Resources</CardTitle>
-        <CardDescription>Resources managed in this environment</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ResourcesPageContent
-          environment={environment}
-          workspaceId={workspace.id}
-        />
-      </CardContent>
-    </Card>
+    <ResourcesPageContent
+      environment={environment}
+      workspaceId={workspace.id}
+    />
   );
 }
