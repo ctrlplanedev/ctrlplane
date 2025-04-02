2553ed33296e7d7cff4ad7f21255c1f2de197cab
Justin Brooks <jsbroks@gmail.com>
2025-04-02 14:13:29 -0400

    init event lib (#440)

---
+++ apps/event-worker/src/workers/new-deployment.ts
@@ -0,0 +1,67 @@
+import type { Tx } from "@ctrlplane/db";
+import type { ResourceCondition } from "@ctrlplane/validators/resources";
+import _ from "lodash";
+
+import { and, eq, isNull } from "@ctrlplane/db";
+import { db } from "@ctrlplane/db/client";
+import * as schema from "@ctrlplane/db/schema";
+import { Channel, createWorker, getQueue } from "@ctrlplane/events";
+
+const getDeploymentResources = async (
+  tx: Tx,
+  deployment: {
+    id: string;
+    systemId: string;
+    resourceSelector?: ResourceCondition | null;
+  },
+) => {
+  const system = await tx.query.system.findFirst({
+    where: eq(schema.system.id, deployment.systemId),
+    with: { environments: true },
+  });
+
+  if (system == null) throw new Error("System or deployment not found");
+
+  const { environments } = system;
+
+  // Simplify the chained operations with standard Promise.all
+  const resources = await Promise.all(
+    environments.map(async (env) => {
+      if (env.resourceSelector == null) return [];
+
+      const res = await tx
+        .select()
+        .from(schema.resource)
+        .where(
+          and(
+            eq(schema.resource.workspaceId, system.workspaceId),
+            isNull(schema.resource.deletedAt),
+            schema.resourceMatchesMetadata(tx, env.resourceSelector),
+            schema.resourceMatchesMetadata(tx, deployment.resourceSelector),
+          ),
+        );
+      return res.map((r) => ({ ...r, environment: env }));
+    }),
+  ).then((arrays) => arrays.flat());
+
+  return resources;
+};
+
+const evaluatedQueue = getQueue(Channel.ReleaseEvaluate);
+
+export const newDeploymentWorker = createWorker(
+  Channel.NewDeployment,
+  async (job) => {
+    const resources = await getDeploymentResources(db, job.data);
+    const jobData = resources.map((r) => {
+      const resourceId = r.id;
+      const environmentId = r.environment.id;
+      const deploymentId = job.data.id;
+      return {
+        name: `${resourceId}-${environmentId}-${deploymentId}`,
+        data: { resourceId, environmentId, deploymentId },
+      };
+    });
+    await evaluatedQueue.addBulk(jobData);
+  },
+);
---
+++ packages/events/src/index.ts
@@ -0,0 +1,30 @@
+import type { Job, WorkerOptions } from "bullmq";
+import { Queue, Worker } from "bullmq";
+
+import type { ChannelMap } from "./types.js";
+import { bullmqRedis } from "./redis.js";
+
+export const createWorker = <T extends keyof ChannelMap>(
+  name: T,
+  handler: (job: Job<ChannelMap[T]>) => Promise<void>,
+  opts?: WorkerOptions,
+) =>
+  new Worker(String(name), handler, {
+    connection: bullmqRedis,
+    removeOnComplete: { age: 1 * 60 * 60, count: 5000 },
+    removeOnFail: { age: 12 * 60 * 60, count: 5000 },
+    concurrency: 100,
+    ...opts,
+  });
+
+const _queues = new Map<keyof ChannelMap, Queue>();
+export const getQueue = <T extends keyof ChannelMap>(name: T) => {
+  if (!_queues.has(name)) {
+    _queues.set(name, new Queue(String(name), { connection: bullmqRedis }));
+  }
+
+  return _queues.get(name) as Queue<ChannelMap[T]>;
+};
+
+export * from "./types.js";
+export * from "./redis.js";
