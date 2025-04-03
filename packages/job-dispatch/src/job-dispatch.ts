455c115aee7f4697a8a8a46e784c2de2b0c1c1df
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2025-04-03 00:59:58 -0500

    fix job status logic

--- packages/job-dispatch/src/job-dispatch.ts
+++ packages/job-dispatch/src/job-dispatch.ts
@@ -99,11 +99,12 @@ class DispatchBuilder {
           data: { jobId: wf.id },
         })),
       );
-      await Promise.all(
-        validJobsWithResolvedVariables.map((j) =>
-          updateJob(this.db, j.id, { status: JobStatus.InProgress }),
-        ),
-      );
+
+      // await Promise.all(
+      //   validJobsWithResolvedVariables.map((j) =>
+      //     updateJob(this.db, j.id, { status: JobStatus.InProgress }),
+      //   ),
+      // );
     }
 
     await Promise.all(
