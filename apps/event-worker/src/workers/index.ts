3485faa4fa5e3376ccd58acd3928a07c8e74e701
Aditya Choudhari <48932219+adityachoudhari26@users.noreply.github.com>
2025-04-05 10:10:36 -0700

    feat: Add variable change workers (#453)

--- apps/event-worker/src/workers/index.ts
+++ apps/event-worker/src/workers/index.ts
@@ -8,6 +8,8 @@ import { dispatchJobWorker } from "./job-dispatch/index.js";
 import { newDeploymentVersionWorker } from "./new-deployment-version.js";
 import { newDeploymentWorker } from "./new-deployment.js";
 import { resourceScanWorker } from "./resource-scan/index.js";
+import { updateDeploymentVariableWorker } from "./update-deployment-variable.js";
+import { updateResourceVariableWorker } from "./update-resource-variable.js";
 
 type Workers<T extends keyof ChannelMap> = {
   [K in T]: Worker<ChannelMap[K]> | null;
@@ -18,6 +20,8 @@ export const workers: Workers<keyof ChannelMap> = {
   [Channel.NewDeploymentVersion]: newDeploymentVersionWorker,
   [Channel.NewEnvironment]: null,
   [Channel.EnvironmentSelectorUpdate]: envSelectorUpdateWorker,
+  [Channel.UpdateDeploymentVariable]: updateDeploymentVariableWorker,
+  [Channel.UpdateResourceVariable]: updateResourceVariableWorker,
   [Channel.EvaluateReleaseTarget]: null,
   [Channel.DispatchJob]: dispatchJobWorker,
   [Channel.ResourceScan]: resourceScanWorker,
