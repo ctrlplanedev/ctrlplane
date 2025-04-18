f8a558aec10e44d326ae3b427a59d8228d739c09
Justin Brooks <justin@wandb.com>
2025-04-18 00:17:02 -0400

    bump turbo

--- packages/validators/src/resources/kubernetes-v1.ts
+++ packages/validators/src/resources/kubernetes-v1.ts
@@ -98,45 +98,10 @@ export const kubernetesDeploymentV1 = z.object({
   kind: z.literal("Deployment"),
   identifier: z.string(),
   name: z.string(),
-  config: clusterConfig.and(
-    z.object({
-      namespace: z.string(),
-      image: z.string(),
-      replicas: z.number().optional(),
-      ports: z
-        .array(
-          z.object({
-            containerPort: z.number(),
-            protocol: z.enum(["TCP", "UDP"]).optional(),
-          }),
-        )
-        .optional(),
-      env: z
-        .array(
-          z.object({
-            name: z.string(),
-            value: z.string().optional(),
-            valueFrom: z
-              .object({
-                configMapKeyRef: z
-                  .object({
-                    name: z.string(),
-                    key: z.string(),
-                  })
-                  .optional(),
-                secretKeyRef: z
-                  .object({
-                    name: z.string(),
-                    key: z.string(),
-                  })
-                  .optional(),
-              })
-              .optional(),
-          }),
-        )
-        .optional(),
-    }),
-  ),
+  config: z.object({
+    name: z.string(),
+    namespace: z.string(),
+  }),
   metadata: z.record(z.string()).and(
     z
       .object({
