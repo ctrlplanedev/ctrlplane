dcc91689bc0d2c156044fcbeda4a3b7bc3b5f5ee
Justin Brooks <justin@wandb.com>
2025-04-23 00:36:24 -0400

    remove compute selector fluent api

--- packages/db/src/selectors/index.ts
+++ packages/db/src/selectors/index.ts
@@ -1,14 +1,9 @@
 import type { Tx } from "../common.js";
 import { db } from "../client.js";
-import { ComputeBuilder } from "./compute/compute.js";
 import { QueryBuilder } from "./query/builder.js";
 
 class InitialBuilder {
   constructor(private readonly tx: Tx) {}
-
-  compute() {
-    return new ComputeBuilder(this.tx);
-  }
 
   query() {
     return new QueryBuilder(this.tx);
