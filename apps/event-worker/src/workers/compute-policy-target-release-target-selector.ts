6fe0ad90aa62d1b272d86d525de674454535389b
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-04-24 15:13:38 -0700

    policy target rts computation

--- apps/event-worker/src/workers/compute-policy-target-release-target-selector.ts
+++ apps/event-worker/src/workers/compute-policy-target-release-target-selector.ts
@@ -1,9 +1,55 @@
-import { and, eq, inArray, isNull, selector } from "@ctrlplane/db";
+import type { Tx } from "@ctrlplane/db";
+
+import { and, eq, inArray, isNull, selector, sql } from "@ctrlplane/db";
 import { db } from "@ctrlplane/db/client";
 import * as schema from "@ctrlplane/db/schema";
 import { Channel, createWorker, getQueue } from "@ctrlplane/events";
 
-import { withMutex } from "../utils/with-mutex.js";
+const findMatchingReleaseTargets = (
+  tx: Tx,
+  policyTarget: schema.PolicyTarget,
+) =>
+  tx
+    .select()
+    .from(schema.releaseTarget)
+    .innerJoin(
+      schema.resource,
+      eq(schema.releaseTarget.resourceId, schema.resource.id),
+    )
+    .innerJoin(
+      schema.deployment,
+      eq(schema.releaseTarget.deploymentId, schema.deployment.id),
+    )
+    .innerJoin(
+      schema.environment,
+      eq(schema.releaseTarget.environmentId, schema.environment.id),
+    )
+    .where(
+      and(
+        isNull(schema.resource.deletedAt),
+        selector()
+          .query()
+          .resources()
+          .where(policyTarget.resourceSelector)
+          .sql(),
+        selector()
+          .query()
+          .deployments()
+          .where(policyTarget.deploymentSelector)
+          .sql(),
+        selector()
+          .query()
+          .environments()
+          .where(policyTarget.environmentSelector)
+          .sql(),
+      ),
+    )
+    .then((rt) =>
+      rt.map((rt) => ({
+        policyTargetId: policyTarget.id,
+        releaseTargetId: rt.release_target.id,
+      })),
+    );
 
 export const computePolicyTargetReleaseTargetSelectorWorkerEvent = createWorker(
   Channel.ComputePolicyTargetReleaseTargetSelector,
@@ -12,13 +58,27 @@ export const computePolicyTargetReleaseTargetSelectorWorkerEvent = createWorker(
 
     const policyTarget = await db.query.policyTarget.findFirst({
       where: eq(schema.policyTarget.id, id),
+      with: { policy: true },
     });
 
     if (policyTarget == null) throw new Error("Policy target not found");
 
-    const key = `${Channel.ComputePolicyTargetReleaseTargetSelector}:${policyTarget.id}`;
-    const [acquired, rts] = await withMutex(key, () =>
-      db.transaction(async (tx) => {
+    const { policy } = policyTarget;
+    const { workspaceId } = policy;
+
+    try {
+      const rts = await db.transaction(async (tx) => {
+        await tx.execute(
+          sql`
+            SELECT * FROM ${schema.system}
+            INNER JOIN ${schema.environment} ON ${eq(schema.environment.systemId, schema.system.id)}
+            INNER JOIN ${schema.deployment} ON ${eq(schema.deployment.systemId, schema.system.id)}
+            INNER JOIN ${schema.releaseTarget} ON ${eq(schema.releaseTarget.environmentId, schema.environment.id)}
+            WHERE ${eq(schema.system.workspaceId, workspaceId)}
+            FOR UPDATE NOWAIT
+          `,
+        );
+
         await tx
           .delete(schema.computedPolicyTargetReleaseTarget)
           .where(
@@ -28,47 +88,10 @@ export const computePolicyTargetReleaseTargetSelectorWorkerEvent = createWorker(
             ),
           );
 
-        const releaseTargets = await tx
-          .select()
-          .from(schema.releaseTarget)
-          .innerJoin(
-            schema.resource,
-            eq(schema.releaseTarget.resourceId, schema.resource.id),
-          )
-          .innerJoin(
-            schema.deployment,
-            eq(schema.releaseTarget.deploymentId, schema.deployment.id),
-          )
-          .innerJoin(
-            schema.environment,
-            eq(schema.releaseTarget.environmentId, schema.environment.id),
-          )
-          .where(
-            and(
-              isNull(schema.resource.deletedAt),
-              selector()
-                .query()
-                .resources()
-                .where(policyTarget.resourceSelector)
-                .sql(),
-              selector()
-                .query()
-                .deployments()
-                .where(policyTarget.deploymentSelector)
-                .sql(),
-              selector()
-                .query()
-                .environments()
-                .where(policyTarget.environmentSelector)
-                .sql(),
-            ),
-          )
-          .then((rt) =>
-            rt.map((rt) => ({
-              policyTargetId: policyTarget.id,
-              releaseTargetId: rt.release_target.id,
-            })),
-          );
+        const releaseTargets = await findMatchingReleaseTargets(
+          tx,
+          policyTarget,
+        );
 
         if (releaseTargets.length === 0) return [];
         return tx
@@ -76,33 +99,35 @@ export const computePolicyTargetReleaseTargetSelectorWorkerEvent = createWorker(
           .values(releaseTargets)
           .onConflictDoNothing()
           .returning();
-      }),
-    );
+      });
 
-    if (!acquired) {
-      await getQueue(Channel.ComputePolicyTargetReleaseTargetSelector).add(
-        job.name,
-        job.data,
-        { deduplication: { id: job.data.id, ttl: 500 } },
-      );
-      return;
+      if (rts.length === 0) return;
+
+      const releaseTargets = await db.query.releaseTarget.findMany({
+        where: inArray(
+          schema.releaseTarget.id,
+          rts.map((rt) => rt.releaseTargetId),
+        ),
+      });
+
+      const jobs = releaseTargets.map((rt) => ({
+        name: `${rt.resourceId}-${rt.environmentId}-${rt.deploymentId}`,
+        data: rt,
+        opts: { deduplication: { id: rt.id, ttl: 500 } },
+      }));
+      await getQueue(Channel.EvaluateReleaseTarget).addBulk(jobs);
+    } catch (e: any) {
+      const isRowLocked = e.code === "55P03";
+      if (isRowLocked) {
+        await getQueue(Channel.ComputePolicyTargetReleaseTargetSelector).add(
+          job.name,
+          job.data,
+          { deduplication: { id: job.data.id, ttl: 500 } },
+        );
+        return;
+      }
+
+      throw e;
     }
-
-    if (rts == null) return;
-    if (rts.length === 0) return;
-
-    const releaseTargets = await db.query.releaseTarget.findMany({
-      where: inArray(
-        schema.releaseTarget.id,
-        rts.map((rt) => rt.releaseTargetId),
-      ),
-    });
-
-    const jobs = releaseTargets.map((rt) => ({
-      name: `${rt.resourceId}-${rt.environmentId}-${rt.deploymentId}`,
-      data: rt,
-      opts: { deduplication: { id: rt.id, ttl: 500 } },
-    }));
-    await getQueue(Channel.EvaluateReleaseTarget).addBulk(jobs);
   },
 );
