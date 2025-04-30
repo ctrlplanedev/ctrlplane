64434d99b2bb5ea91dab4cbc542ec9ddb5f4eb46
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-04-30 15:13:33 -0700

    update relations

--- packages/db/src/schema/rules/rule-relations.ts
+++ packages/db/src/schema/rules/rule-relations.ts
@@ -3,26 +3,16 @@ import { relations } from "drizzle-orm";
 import { user } from "../auth.js";
 import { deploymentVersion } from "../deployment-version.js";
 import { policy } from "../policy.js";
-import {
-  policyRuleAnyApproval,
-  policyRuleAnyApprovalRecord,
-} from "./approval-any.js";
-import {
-  policyRuleRoleApproval,
-  policyRuleRoleApprovalRecord,
-} from "./approval-role.js";
-import {
-  policyRuleUserApproval,
-  policyRuleUserApprovalRecord,
-} from "./approval-user.js";
+import { policyRuleAnyApproval } from "./approval-any.js";
+import { deploymentVersionApprovalRecord } from "./approval-record.js";
+import { policyRuleRoleApproval } from "./approval-role.js";
+import { policyRuleUserApproval } from "./approval-user.js";
 import { policyRuleDenyWindow } from "./deny-window.js";
 import { policyRuleDeploymentVersionSelector } from "./deployment-selector.js";
 
 // User relations to approval records
 export const userApprovalRelations = relations(user, ({ many }) => ({
-  userApprovalRecords: many(policyRuleUserApprovalRecord),
-  roleApprovalRecords: many(policyRuleRoleApprovalRecord),
-  anyApprovalRecords: many(policyRuleAnyApprovalRecord),
+  approvalRecords: many(deploymentVersionApprovalRecord),
 }));
 
 // Approval user relations
@@ -32,24 +22,6 @@ export const policyRuleUserApprovalRelations = relations(
     policy: one(policy, {
       fields: [policyRuleUserApproval.policyId],
       references: [policy.id],
-    }),
-  }),
-);
-
-export const policyRuleUserApprovalRecordRelations = relations(
-  policyRuleUserApprovalRecord,
-  ({ one }) => ({
-    user: one(user, {
-      fields: [policyRuleUserApprovalRecord.userId],
-      references: [user.id],
-    }),
-    deploymentVersion: one(deploymentVersion, {
-      fields: [policyRuleUserApprovalRecord.deploymentVersionId],
-      references: [deploymentVersion.id],
-    }),
-    rule: one(policyRuleUserApproval, {
-      fields: [policyRuleUserApprovalRecord.ruleId],
-      references: [policyRuleUserApproval.id],
     }),
   }),
 );
@@ -65,24 +37,6 @@ export const policyRuleRoleApprovalRelations = relations(
   }),
 );
 
-export const policyRuleRoleApprovalRecordRelations = relations(
-  policyRuleRoleApprovalRecord,
-  ({ one }) => ({
-    user: one(user, {
-      fields: [policyRuleRoleApprovalRecord.userId],
-      references: [user.id],
-    }),
-    deploymentVersion: one(deploymentVersion, {
-      fields: [policyRuleRoleApprovalRecord.deploymentVersionId],
-      references: [deploymentVersion.id],
-    }),
-    rule: one(policyRuleRoleApproval, {
-      fields: [policyRuleRoleApprovalRecord.ruleId],
-      references: [policyRuleRoleApproval.id],
-    }),
-  }),
-);
-
 // Approval any relations
 export const policyRuleAnyApprovalRelations = relations(
   policyRuleAnyApproval,
@@ -90,20 +44,6 @@ export const policyRuleAnyApprovalRelations = relations(
     policy: one(policy, {
       fields: [policyRuleAnyApproval.policyId],
       references: [policy.id],
-    }),
-  }),
-);
-
-export const policyRuleAnyApprovalRecordRelations = relations(
-  policyRuleAnyApprovalRecord,
-  ({ one }) => ({
-    user: one(user, {
-      fields: [policyRuleAnyApprovalRecord.userId],
-      references: [user.id],
-    }),
-    deploymentVersion: one(deploymentVersion, {
-      fields: [policyRuleAnyApprovalRecord.deploymentVersionId],
-      references: [deploymentVersion.id],
     }),
   }),
 );
@@ -124,6 +64,20 @@ export const policyDeploymentVersionSelectorRelations = relations(
     policy: one(policy, {
       fields: [policyRuleDeploymentVersionSelector.policyId],
       references: [policy.id],
+    }),
+  }),
+);
+
+export const deploymentVersionApprovalRecordRelations = relations(
+  deploymentVersionApprovalRecord,
+  ({ one }) => ({
+    deploymentVersion: one(deploymentVersion, {
+      fields: [deploymentVersionApprovalRecord.deploymentVersionId],
+      references: [deploymentVersion.id],
+    }),
+    user: one(user, {
+      fields: [deploymentVersionApprovalRecord.userId],
+      references: [user.id],
     }),
   }),
 );
