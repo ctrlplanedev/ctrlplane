759ae71834969ef718ef4d49635ceefcc8f77569
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-05-05 11:38:01 -0700

    feat: resource diff view when editing env resource selector

--- apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/resources/_components/EditSelector.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(app)/(deploy)/(raw)/systems/[systemSlug]/(raw)/environments/[environmentId]/resources/_components/EditSelector.tsx
@@ -8,11 +8,13 @@ import { useRouter } from "next/navigation";
 import { Button } from "@ctrlplane/ui/button";
 
 import { ResourceConditionDialog } from "~/app/[workspaceSlug]/(app)/_components/resources/condition/ResourceConditionDialog";
+import { ResourceList } from "~/app/[workspaceSlug]/(app)/_components/resources/condition/ResourceList";
 import { api } from "~/trpc/react";
 
 export const EditSelector: React.FC<{
   environment: SCHEMA.Environment;
-}> = ({ environment }) => {
+  resources: SCHEMA.Resource[];
+}> = ({ environment, resources }) => {
   const updateEnvironment = api.environment.update.useMutation();
   const router = useRouter();
 
@@ -28,7 +30,35 @@ export const EditSelector: React.FC<{
     <ResourceConditionDialog
       condition={environment.resourceSelector}
       onChange={onChange}
-      showResourceList
+      ResourceList={({ filter }) => (
+        <ResourceList
+          filter={filter}
+          ResourceDiff={({ newResources }) => {
+            const addedResources = newResources.filter(
+              (r) => !resources.some((c) => c.id === r.id),
+            );
+
+            const removedResources = resources.filter(
+              (r) => !newResources.some((c) => c.id === r.id),
+            );
+
+            return (
+              <div className="text-xs">
+                {addedResources.length > 0 && (
+                  <div className="text-green-400">
+                    +{addedResources.length} new resources
+                  </div>
+                )}
+                {removedResources.length > 0 && (
+                  <div className="text-red-400">
+                    -{removedResources.length} resources
+                  </div>
+                )}
+              </div>
+            );
+          }}
+        />
+      )}
     >
       <Button size="sm" variant="outline">
         Edit Selector
