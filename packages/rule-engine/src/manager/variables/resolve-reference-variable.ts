01b641fac9c42c02bb511382b456182b58bf12da
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-05-09 22:33:19 -0700

    rabbit comment

--- packages/rule-engine/src/manager/variables/resolve-reference-variable.ts
+++ packages/rule-engine/src/manager/variables/resolve-reference-variable.ts
@@ -3,19 +3,25 @@ import _ from "lodash";
 
 import { db } from "@ctrlplane/db/client";
 import { getResourceParents } from "@ctrlplane/db/queries";
+import { logger } from "@ctrlplane/logger";
 
 export const getReferenceVariableValue = async (
   variable: schema.ReferenceResourceVariable,
 ) => {
-  const { relationships, getTargetsWithMetadata } = await getResourceParents(
-    db,
-    variable.resourceId,
-  );
-  const relationshipTargets = await getTargetsWithMetadata();
+  try {
+    const { relationships, getTargetsWithMetadata } = await getResourceParents(
+      db,
+      variable.resourceId,
+    );
+    const relationshipTargets = await getTargetsWithMetadata();
 
-  const targetId = relationships[variable.reference]?.target.id ?? "";
-  const targetResource = relationshipTargets[targetId];
-  if (targetResource == null) return variable.defaultValue;
+    const targetId = relationships[variable.reference]?.target.id ?? "";
+    const targetResource = relationshipTargets[targetId];
+    if (targetResource == null) return variable.defaultValue;
 
-  return _.get(targetResource, variable.path, variable.defaultValue);
+    return _.get(targetResource, variable.path, variable.defaultValue);
+  } catch (error) {
+    logger.error("Error resolving reference variable", { error, variable });
+    return variable.defaultValue;
+  }
 };
