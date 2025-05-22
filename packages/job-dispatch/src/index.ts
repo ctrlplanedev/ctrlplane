02778e030c69ae5bdf27ad9edfdcf4e1eef35802
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-05-21 17:01:27 -0700

    chore: remove legacy policy checkers

--- packages/job-dispatch/src/index.ts
+++ packages/job-dispatch/src/index.ts
@@ -3,22 +3,11 @@ export * from "./release-job-trigger.js";
 export * from "./deployment-update.js";
 export * from "./job-update.js";
 export * from "./job-dispatch.js";
-export * from "./policy-checker.js";
 export * from "./policy-create.js";
 export * from "./release-sequencing.js";
-export * from "./lock-checker.js";
 export * from "./cancel-previous-jobs.js";
 
 export { isDateInTimeWindow } from "./utils.js";
 
-export * from "./policies/gradual-rollout.js";
-export * from "./policies/manual-approval.js";
-export * from "./policies/release-sequencing.js";
-export * from "./policies/success-rate-criteria-passing.js";
-export * from "./policies/release-dependency.js";
-export * from "./policies/release-string-check.js";
-export * from "./policies/concurrency-policy.js";
-export * from "./policies/release-window.js";
-export * from "./pending-job-checker.js";
 export * from "./events/index.js";
 export * from "./resource/index.js";
