47e81278762b48a1956efa9f859ae62ead3b691f
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-06-02 15:59:45 -0700

    chore: bump concurrency of updated resource worker

--- apps/event-worker/src/workers/updated-resources/index.ts
+++ apps/event-worker/src/workers/updated-resources/index.ts
@@ -59,5 +59,5 @@ export const updatedResourceWorker = createWorker(
     const affectedReleaseTargets = await getAffectedReleaseTargets(resource.id);
     await dispatchEvaluateJobs(affectedReleaseTargets);
   }),
-  { concurrency: 10 },
+  { concurrency: 50 },
 );
