e6a968e4479d2d4e78bcb0d737cbc91e110e2c54
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-06-16 11:59:06 -0700

    chore: add error log if body parser fails

--- apps/webservice/src/app/api/v1/body-parser.tsx
+++ apps/webservice/src/app/api/v1/body-parser.tsx
@@ -1,14 +1,21 @@
 import type { z } from "zod";
 import { NextResponse } from "next/server";
 
+import { logger } from "@ctrlplane/logger";
+
 import type { Middleware } from "./middleware";
+
+const log = logger.child({ module: "body-parser" });
 
 export const parseBody: <T extends z.ZodTypeAny>(schema: T) => Middleware =
   (schema) => async (ctx, _, next) => {
     const response = await ctx.req.json();
 
     const body = schema.safeParse(response);
-    if (!body.success) return NextResponse.json(body.error, { status: 400 });
+    if (!body.success) {
+      log.error("Failed to parse body", { error: body.error });
+      return NextResponse.json(body.error, { status: 400 });
+    }
 
     ctx.body = body.data;
     return next(ctx);
