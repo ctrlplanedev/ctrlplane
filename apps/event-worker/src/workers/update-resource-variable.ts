25751792c0a7a20987c0447065b46251663cf660
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-06-23 15:59:00 -0700

    fix: dispatch evaluate jobs for resource release targets in update resource variable worker

--- apps/event-worker/src/workers/update-resource-variable.ts
+++ apps/event-worker/src/workers/update-resource-variable.ts
@@ -6,35 +6,57 @@ import { getResourceChildren } from "@ctrlplane/db/queries";
 import * as schema from "@ctrlplane/db/schema";
 import { Channel, createWorker, dispatchQueueJob } from "@ctrlplane/events";
 
+const getResourceReleaseTargets = async (resourceId: string) =>
+  db.query.releaseTarget.findMany({
+    where: eq(schema.releaseTarget.resourceId, resourceId),
+  });
+
+const getResourceChildrenReleaseTargets = async (
+  resourceId: string,
+  key: string,
+) => {
+  const dependentResources = await getResourceChildren(db, resourceId);
+
+  return db
+    .selectDistinctOn([schema.releaseTarget.id])
+    .from(schema.releaseTarget)
+    .innerJoin(
+      schema.deployment,
+      eq(schema.releaseTarget.deploymentId, schema.deployment.id),
+    )
+    .innerJoin(
+      schema.deploymentVariable,
+      eq(schema.deploymentVariable.deploymentId, schema.deployment.id),
+    )
+    .where(
+      and(
+        eq(schema.deploymentVariable.key, key),
+        inArray(
+          schema.releaseTarget.resourceId,
+          dependentResources.map((dr) => dr.source.id),
+        ),
+      ),
+    )
+    .then((rows) => rows.map((row) => row.release_target));
+};
+
 export const updateResourceVariableWorker = createWorker(
   Channel.UpdateResourceVariable,
   async (job) => {
     const { data: variable } = job;
     const { resourceId, key } = variable;
 
-    const dependentResources = await getResourceChildren(db, resourceId);
+    const resourceReleaseTargets = await getResourceReleaseTargets(resourceId);
 
-    const affectedReleaseTargets = await db
-      .selectDistinctOn([schema.releaseTarget.id])
-      .from(schema.releaseTarget)
-      .innerJoin(
-        schema.deployment,
-        eq(schema.releaseTarget.deploymentId, schema.deployment.id),
-      )
-      .innerJoin(
-        schema.deploymentVariable,
-        eq(schema.deploymentVariable.deploymentId, schema.deployment.id),
-      )
-      .where(
-        and(
-          eq(schema.deploymentVariable.key, key),
-          inArray(
-            schema.releaseTarget.resourceId,
-            dependentResources.map((dr) => dr.source.id),
-          ),
-        ),
-      )
-      .then((rows) => rows.map((row) => row.release_target));
+    const childrenReleaseTargets = await getResourceChildrenReleaseTargets(
+      resourceId,
+      key,
+    );
+
+    const affectedReleaseTargets = [
+      ...resourceReleaseTargets,
+      ...childrenReleaseTargets,
+    ];
 
     dispatchQueueJob().toEvaluate().releaseTargets(affectedReleaseTargets);
   },
