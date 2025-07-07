869573f3b73b99673701ae0a60770e28c9b74268
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-07-07 16:44:09 -0400

    chore: error logging to latest release target jobs route

--- apps/webservice/src/app/api/v1/release-targets/[releaseTargetId]/latest-jobs/route.ts
+++ apps/webservice/src/app/api/v1/release-targets/[releaseTargetId]/latest-jobs/route.ts
@@ -1,15 +1,20 @@
 import type { Tx } from "@ctrlplane/db";
 import { NextResponse } from "next/server";
-import { NOT_FOUND } from "http-status";
+import { INTERNAL_SERVER_ERROR, NOT_FOUND } from "http-status";
 import { isPresent } from "ts-is-present";
 
 import { desc, eq, takeFirstOrNull } from "@ctrlplane/db";
 import * as schema from "@ctrlplane/db/schema";
+import { logger } from "@ctrlplane/logger";
 import { Permission } from "@ctrlplane/validators/auth";
 
 import { authn, authz } from "~/app/api/v1/auth";
 import { getJob } from "~/app/api/v1/jobs/[jobId]/get-job";
 import { request } from "~/app/api/v1/middleware";
+
+const log = logger.child({
+  route: "/v1/release-targets/[releaseTargetId]/latest-jobs",
+});
 
 const getReleaseTarget = async (db: Tx, releaseTargetId: string) =>
   db
@@ -47,21 +52,31 @@ export const GET = request()
   )
   .handle<object, { params: Promise<{ releaseTargetId: string }> }>(
     async ({ db }, { params }) => {
-      const { releaseTargetId } = await params;
+      try {
+        const { releaseTargetId } = await params;
 
-      const releaseTarget = await getReleaseTarget(db, releaseTargetId);
-      if (releaseTarget == null)
+        const releaseTarget = await getReleaseTarget(db, releaseTargetId);
+        if (releaseTarget == null)
+          return NextResponse.json(
+            { error: "Release target not found" },
+            { status: NOT_FOUND },
+          );
+
+        const latestJobs = await getLatestJobs(db, releaseTargetId);
+
+        const fullJobs = await Promise.all(
+          latestJobs.map((job) => getJob(db, job.id)),
+        ).then((jobs) => jobs.filter(isPresent));
+
+        return NextResponse.json(fullJobs);
+      } catch (error) {
+        log.error("Error getting latest jobs", {
+          error,
+        });
         return NextResponse.json(
-          { error: "Release target not found" },
-          { status: NOT_FOUND },
+          { error: "Failed to get latest jobs" },
+          { status: INTERNAL_SERVER_ERROR },
         );
-
-      const latestJobs = await getLatestJobs(db, releaseTargetId);
-
-      const fullJobs = await Promise.all(
-        latestJobs.map((job) => getJob(db, job.id)),
-      ).then((jobs) => jobs.filter(isPresent));
-
-      return NextResponse.json(fullJobs);
+      }
     },
   );
