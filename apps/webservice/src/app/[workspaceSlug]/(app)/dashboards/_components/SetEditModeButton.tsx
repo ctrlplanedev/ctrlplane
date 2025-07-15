295be336c47aca81f6fa5656e6cd43d1c1131ac4
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-07-14 17:00:05 -0700

    chore: cleanup set edit mode button

--- apps/webservice/src/app/[workspaceSlug]/(app)/dashboards/_components/SetEditModeButton.tsx
+++ apps/webservice/src/app/[workspaceSlug]/(app)/dashboards/_components/SetEditModeButton.tsx
@@ -2,18 +2,23 @@
 
 import { Button } from "@ctrlplane/ui/button";
 
+import { useEditingWidget } from "../[dashboardId]/_hooks/useEditingWidget";
 import { useDashboard } from "../[dashboardId]/DashboardContext";
 
 export const SetEditModeButton: React.FC = () => {
-  const { isEditMode, setIsEditMode } = useDashboard();
+  const { isEditMode: isCurrentlyEditMode, setIsEditMode } = useDashboard();
+  const { clearEditingWidget } = useEditingWidget();
 
   return (
     <Button
       variant="outline"
       size="sm"
-      onClick={() => setIsEditMode(!isEditMode)}
+      onClick={() => {
+        setIsEditMode(!isCurrentlyEditMode);
+        if (isCurrentlyEditMode) clearEditingWidget();
+      }}
     >
-      {isEditMode ? "Exit edit mode" : "Edit"}
+      {isCurrentlyEditMode ? "Exit edit mode" : "Edit"}
     </Button>
   );
 };
