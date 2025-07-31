7dd0daa97922605fb4518edc82e56d768f12eba8
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-07-31 16:24:29 -0700

    fix nextjs build

--- packages/events/src/kafka/client.ts
+++ packages/events/src/kafka/client.ts
@@ -1,19 +1,33 @@
+import type { Producer } from "kafkajs";
 import { Kafka } from "kafkajs";
 
 import type { EventPayload, Message } from "./events.js";
 import { env } from "../config.js";
 
-const kafka = new Kafka({
-  clientId: "ctrlplane-events",
-  brokers: env.KAFKA_BROKERS.split(","),
-});
+let kafka: Kafka | null = null;
+let producer: Producer | null = null;
 
-const producer = kafka.producer();
+const getKafka = () => {
+  kafka ??= new Kafka({
+    clientId: "ctrlplane-events",
+    brokers: env.KAFKA_BROKERS.split(","),
+  });
+  return kafka;
+};
+
+const getProducer = async () => {
+  if (producer == null) {
+    producer = getKafka().producer();
+    await producer.connect();
+  }
+  return producer;
+};
 
 export const sendEvent = async <T extends keyof EventPayload>(
   message: Message<T> | Message<T>[],
 ) => {
   const messages = Array.isArray(message) ? message : [message];
+  const producer = await getProducer();
   await producer.send({
     topic: "ctrlplane-events",
     messages: messages.map((message) => ({
