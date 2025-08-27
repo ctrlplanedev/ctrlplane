20ae829fb2238c666299cf3b0638ff9998ee475d
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-08-27 11:06:52 -0700

    Remove RepositoryWithID

--- apps/event-queue/src/repository/repository.ts
+++ apps/event-queue/src/repository/repository.ts
@@ -8,32 +8,3 @@ export interface Repository<T extends Entity> {
   delete(id: string): Promise<T | null> | T | null;
   exists(id: string): Promise<boolean> | boolean;
 }
-
-export class RepositoryWithID<T extends Entity> implements Repository<T> {
-  private entities: Record<string, T> = {};
-
-  get(id: string): T | null {
-    return this.entities[id] ?? null;
-  }
-  getAll(): T[] {
-    return Object.values(this.entities);
-  }
-  create(entity: T): T {
-    this.entities[entity.id] = entity;
-    return entity;
-  }
-  update(entity: T): T {
-    const existing = this.entities[entity.id] ?? {};
-    this.entities[entity.id] = { ...existing, ...entity };
-    return this.entities[entity.id]!;
-  }
-  delete(id: string): T | null {
-    const entity = this.entities[id];
-    if (entity == null) return null;
-    delete this.entities[id];
-    return entity;
-  }
-  exists(id: string): boolean {
-    return this.entities[id] != null;
-  }
-}
