3b1d8ecda0e9b69168742e62f761bb7f1f1fa554
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-09-24 16:53:22 -0400

    init

--- apps/event-queue/src/workspace/release-targets/evaluate/variables/common.ts
+++ apps/event-queue/src/workspace/release-targets/evaluate/variables/common.ts
@@ -1,3 +1,6 @@
+import type { Workspace } from "@ctrlplane/db/schema";
+import type { FullResource } from "@ctrlplane/events";
+
 import { variablesAES256 } from "@ctrlplane/secrets";
 
 type DirectVariable = {
@@ -10,3 +13,17 @@ export const resolveDirectVariableValue = (variable: DirectVariable) => {
     typeof value === "object" ? JSON.stringify(value) : String(value);
   return sensitive ? variablesAES256().decrypt(strVal) : value;
 };
+
+type ReferenceVariable = {
+  reference: string;
+  path: string[];
+  defaultValue: string | number | boolean | object | null;
+};
+
+export class ReferenceVariableResolver {
+  constructor(private readonly workspace: Workspace) {}
+
+  async resolve(variable: ReferenceVariable, resource: FullResource) {
+    const { reference, path, defaultValue } = variable;
+  }
+}
--- apps/event-queue/src/workspace/release-targets/evaluate/variables/resource-variables.ts
+++ apps/event-queue/src/workspace/release-targets/evaluate/variables/resource-variables.ts
@@ -8,7 +8,19 @@ export class ResourceVariableProvider {
     private readonly releaseTarget: FullReleaseTarget,
   ) {}
 
+  private async getResourceVariable(key: string) {
+    const { resource } = this.releaseTarget;
+    const allVariables = await this.workspace.repository.resourceVariableRepository.getAll();
+    return allVariables.find((v) => v.resourceId === resource.id && v.key === key);
+  }
+
   async getValue(
     key: string,
-  ): Promise<string | number | boolean | object | null> {}
+  ): Promise<string | number | boolean | object | null | undefined> {
+    const resourceVariable = await this.getResourceVariable(key);
+    if (resourceVariable == null) return undefined;
+
+    if (resourceVariable.valueType === "direct")
+
+  }
 }
