dbe50eb7d9c844494c7c9713b7624450ca05752c
Justin <justin@wandb.com>
2025-10-01 20:53:20 -0400

    fix WORKSPACE_ENGINE_PORT correction

--- apps/event-queue/src/config.ts
+++ apps/event-queue/src/config.ts
@@ -31,14 +31,20 @@ export const env = createEnv({
     GITHUB_BOT_PRIVATE_KEY: z.string().optional(),
     GITHUB_BOT_CLIENT_ID: z.string().optional(),
     GITHUB_BOT_CLIENT_SECRET: z.string().optional(),
-    PORT: z.number().default(3124),
+    PORT: z
+      .string()
+      .default("3124")
+      .transform((val) => parseInt(val)),
 
     WORKSPACE_ENGINE_STATEFUL_SET_NAME: z.string().default("workspace-engine"),
     WORKSPACE_ENGINE_HEADLESS_SERVICE: z
       .string()
       .default("workspace-engine-headless"),
     WORKSPACE_ENGINE_NAMESPACE: z.string().default("ctrlplane"),
-    WORKSPACE_ENGINE_PORT: z.number().default(50051),
+    WORKSPACE_ENGINE_PORT: z
+      .string()
+      .default("8081")
+      .transform((val) => parseInt(val)),
   },
   runtimeEnv: process.env,
 });
