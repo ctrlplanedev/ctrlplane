574a519b34b145074350a22d9066cc53f5dc2ce2
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-10-09 21:55:14 -0700

    chore: add back sdk

---
+++ packages/workspace-engine-sdk/src/index.ts
@@ -0,0 +1,16 @@
+import createOClient, { ClientOptions } from "openapi-fetch";
+
+import { paths } from "./schema";
+
+export { operations as Operations } from "./schema";
+
+export function createClient(options: ClientOptions) {
+  return createOClient<paths>({
+    baseUrl: options.baseUrl,
+    ...options,
+    fetch: (input: Request) => {
+      const url = new URL(input.url);
+      return fetch(new Request(url.toString(), input));
+    },
+  });
+}
