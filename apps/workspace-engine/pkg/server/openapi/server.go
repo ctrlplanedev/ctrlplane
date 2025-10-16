e85b2724800160f90319f4a21b4828e3aad7f499
Justin <justin@wandb.com>
2025-10-16 18:15:04 -0400

    not implemented endpoint

--- apps/workspace-engine/pkg/server/openapi/server.go
+++ apps/workspace-engine/pkg/server/openapi/server.go
@@ -1,6 +1,7 @@
 package openapi
 
 import (
+	"net/http"
 	"workspace-engine/pkg/oapi"
 	"workspace-engine/pkg/server/openapi/deployments"
 	"workspace-engine/pkg/server/openapi/environments"
@@ -23,37 +24,9 @@ type Server struct {
 	policies.Policies
 }
 
-// EvaluateReleaseTarget implements oapi.ServerInterface.
-// Subtle: this method shadows the method (ReleaseTargets).EvaluateReleaseTarget of Server.ReleaseTargets.
-func (s *Server) EvaluateReleaseTarget(c *gin.Context, workspaceId oapi.WorkspaceId) {
-	panic("unimplemented")
-}
-
-// GetDeploymentResources implements oapi.ServerInterface.
-// Subtle: this method shadows the method (Deployments).GetDeploymentResources of Server.Deployments.
-func (s *Server) GetDeploymentResources(c *gin.Context, workspaceId oapi.WorkspaceId, deploymentId oapi.DeploymentId) {
-	panic("unimplemented")
-}
-
-// GetEnvironmentResources implements oapi.ServerInterface.
-// Subtle: this method shadows the method (Environments).GetEnvironmentResources of Server.Environments.
-func (s *Server) GetEnvironmentResources(c *gin.Context, workspaceId oapi.WorkspaceId, environmentId oapi.EnvironmentId) {
-	panic("unimplemented")
-}
-
-// GetPoliciesForReleaseTarget implements oapi.ServerInterface.
-// Subtle: this method shadows the method (ReleaseTargets).GetPoliciesForReleaseTarget of Server.ReleaseTargets.
-func (s *Server) GetPoliciesForReleaseTarget(c *gin.Context, workspaceId oapi.WorkspaceId, releaseTargetId oapi.ReleaseTargetId) {
-	panic("unimplemented")
-}
-
 // GetRelatedEntities implements oapi.ServerInterface.
 func (s *Server) GetRelatedEntities(c *gin.Context, workspaceId oapi.WorkspaceId, entityType oapi.GetRelatedEntitiesParamsEntityType, entityId oapi.EntityId) {
-	panic("unimplemented")
-}
-
-// GetReleaseTargetsForPolicy implements oapi.ServerInterface.
-// Subtle: this method shadows the method (Policies).GetReleaseTargetsForPolicy of Server.Policies.
-func (s *Server) GetReleaseTargetsForPolicy(c *gin.Context, workspaceId oapi.WorkspaceId, policyId oapi.PolicyId) {
-	panic("unimplemented")
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"error": "Not implemented",
+	})
 }
