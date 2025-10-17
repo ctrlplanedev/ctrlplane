f4fe4c1d636557829a852fffb0aec308543e1db7
Justin <justin@wandb.com>
2025-10-16 20:00:38 -0400

    push up clean up

--- apps/workspace-engine/pkg/workspace/populate_workspace.go
+++ apps/workspace-engine/pkg/workspace/populate_workspace.go
@@ -12,46 +12,46 @@ func PopulateWorkspaceWithInitialState(ctx context.Context, ws *Workspace) error
 	}
 
 	for _, system := range initialWorkspaceState.Systems() {
-		if err := ws.Store().Systems.Upsert(ctx, system); err != nil {
+		if err := ws.Systems().Upsert(ctx, system); err != nil {
 			return err
 		}
 	}
 	for _, resource := range initialWorkspaceState.Resources() {
-		if _, err := ws.Store().Resources.Upsert(ctx, resource); err != nil {
+		if _, err := ws.Resources().Upsert(ctx, resource); err != nil {
 			return err
 		}
 	}
 	for _, deployment := range initialWorkspaceState.Deployments() {
-		if err := ws.Store().Deployments.Upsert(ctx, deployment); err != nil {
+		if err := ws.Deployments().Upsert(ctx, deployment); err != nil {
 			return err
 		}
 	}
 	for _, deploymentVersion := range initialWorkspaceState.DeploymentVersions() {
-		ws.Store().DeploymentVersions.Upsert(ctx, deploymentVersion.Id, deploymentVersion)
+		ws.DeploymentVersions().Upsert(ctx, deploymentVersion.Id, deploymentVersion)
 	}
 	for _, deploymentVariable := range initialWorkspaceState.DeploymentVariables() {
-		ws.Store().DeploymentVariables.Upsert(ctx, deploymentVariable.Id, deploymentVariable)
+		ws.DeploymentVariables().Upsert(ctx, deploymentVariable.Id, deploymentVariable)
 	}
 	for _, environment := range initialWorkspaceState.Environments() {
-		if err := ws.Store().Environments.Upsert(ctx, environment); err != nil {
+		if err := ws.Environments().Upsert(ctx, environment); err != nil {
 			return err
 		}
 	}
 	for _, policy := range initialWorkspaceState.Policies() {
-		if err := ws.Store().Policies.Upsert(ctx, policy); err != nil {
+		if err := ws.Policies().Upsert(ctx, policy); err != nil {
 			return err
 		}
 	}
 	for _, release := range initialWorkspaceState.Releases() {
-		if err := ws.Store().Releases.Upsert(ctx, release); err != nil {
+		if err := ws.Releases().Upsert(ctx, release); err != nil {
 			return err
 		}
 	}
 	for _, jobAgent := range initialWorkspaceState.JobAgents() {
-		ws.Store().JobAgents.Upsert(ctx, jobAgent)
+		ws.JobAgents().Upsert(ctx, jobAgent)
 	}
 	for _, relationship := range initialWorkspaceState.Relationships() {
-		if err := ws.Store().Relationships.Upsert(ctx, relationship); err != nil {
+		if err := ws.RelationshipRules().Upsert(ctx, relationship); err != nil {
 			return err
 		}
 	}
