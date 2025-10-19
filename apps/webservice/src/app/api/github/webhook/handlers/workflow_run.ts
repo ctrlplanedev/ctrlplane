691f05cf6b4bd41e5882258f6fb80b16bb84133d
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-10-18 20:21:50 -0700

    chore: remove noisy log

--- apps/webservice/src/app/api/github/webhook/handlers/workflow_run.ts
+++ apps/webservice/src/app/api/github/webhook/handlers/workflow_run.ts
@@ -4,11 +4,8 @@ import { buildConflictUpdateColumns, eq, takeFirstOrNull } from "@ctrlplane/db";
 import { db } from "@ctrlplane/db/client";
 import * as schema from "@ctrlplane/db/schema";
 import { updateJob } from "@ctrlplane/job-dispatch";
-import { logger } from "@ctrlplane/logger";
 import { ReservedMetadataKey } from "@ctrlplane/validators/conditions";
 import { exitedStatus, JobStatus } from "@ctrlplane/validators/jobs";
-
-const log = logger.child({ module: "github-webhook" });
 
 type Conclusion = Exclude<WorkflowRunEvent["workflow_run"]["conclusion"], null>;
 const convertConclusion = (conclusion: Conclusion): schema.JobStatus => {
@@ -68,11 +65,6 @@ const updateLinks = async (jobId: string, links: Record<string, string>) =>
     });
 
 export const handleWorkflowWebhookEvent = async (event: WorkflowRunEvent) => {
-  log.info("Handling github workflow run event", {
-    externalId: event.workflow_run.id,
-    event,
-  });
-
   const {
     id,
     status: externalStatus,
