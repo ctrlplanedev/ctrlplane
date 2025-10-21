5d0d8c43a26602a551c31a54332bfe37d463f69d
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-10-21 16:35:17 -0700

    chore: fix migration

--- packages/db/drizzle/0130_striped_komodo.sql
+++ packages/db/drizzle/0130_striped_komodo.sql
@@ -46,7 +46,7 @@ ALTER TABLE "session" ADD COLUMN "id" uuid PRIMARY KEY DEFAULT gen_random_uuid()
 ALTER TABLE "session" ADD COLUMN "ip_address" text;--> statement-breakpoint
 ALTER TABLE "session" ADD COLUMN "user_agent" text;--> statement-breakpoint
 ALTER TABLE "session" ADD COLUMN "created_at" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "session" ADD COLUMN "updated_at" timestamp with time zone NOT NULL;--> statement-breakpoint
+ALTER TABLE "session" ADD COLUMN "updated_at" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
 ALTER TABLE "user" ADD COLUMN "updated_at" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
 ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "session" ADD CONSTRAINT "session_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
