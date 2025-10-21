69a9d60069858bae16c3a89775fb6288ceeb9cc2
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-10-21 15:10:43 -0700

    fix export

--- packages/auth/src/better/client.ts
+++ packages/auth/src/better/client.ts
@@ -3,4 +3,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient();
-export const { signIn, signOut, useSession } = authClient;
+export const signIn = authClient.signIn;
+export const signOut = authClient.signOut;
+export const useSession = authClient.useSession;
