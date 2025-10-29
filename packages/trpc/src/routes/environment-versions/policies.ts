cb1a49783fa56cd4bf9a555e13b77a0289994f27
Aditya Choudhari <adityachoudhari26@gmail.com>
2025-10-29 13:26:36 -0700

    chore: fetch all releas targets for environment and deployment

--- packages/trpc/src/routes/environment-versions/policies.ts
+++ packages/trpc/src/routes/environment-versions/policies.ts
@@ -1,6 +1,24 @@
 import { z } from "zod";
 
+import { getClientFor } from "@ctrlplane/workspace-engine-sdk";
+
 import { protectedProcedure } from "../../trpc.js";
+import { getDeploymentVersion } from "./util.js";
+
+const getAllReleaseTargets = async (
+  workspaceId: string,
+  environmentId: string,
+  deploymentId: string,
+) => {
+  const response = await getClientFor(workspaceId).GET(
+    "/v1/workspaces/{workspaceId}/environments/{environmentId}/release-targets",
+    { params: { path: { workspaceId, environmentId } } },
+  );
+  if (response.error?.error != null) throw new Error(response.error.error);
+  const envTargets = response.data?.items ?? [];
+
+  return envTargets.filter((target) => target.deployment.id === deploymentId);
+};
 
 export const policies = protectedProcedure
   .input(
@@ -10,6 +28,17 @@ export const policies = protectedProcedure
       versionId: z.uuid(),
     }),
   )
-  .query(async () => {
+  .query(async ({ input }) => {
+    const { workspaceId, environmentId, versionId } = input;
+    const version = await getDeploymentVersion(workspaceId, versionId);
+    const { deploymentId } = version;
+    const releaseTargets = await getAllReleaseTargets(
+      workspaceId,
+      environmentId,
+      deploymentId,
+    );
+
+    console.log(releaseTargets);
+
     return Promise.resolve([]);
   });
