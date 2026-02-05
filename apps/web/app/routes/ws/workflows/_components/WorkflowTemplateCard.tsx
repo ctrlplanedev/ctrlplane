9e3dcd5e9448d1925440919c0dbab9c87069af71
Aditya Choudhari <adityachoudhari26@gmail.com>
2026-02-05 09:14:36 -0800

    chore: basic ui to show workflow state

--- apps/web/app/routes/ws/workflows/_components/WorkflowTemplateCard.tsx
+++ apps/web/app/routes/ws/workflows/_components/WorkflowTemplateCard.tsx
@@ -1,11 +1,8 @@
 import type { WorkspaceEngine } from "@ctrlplane/workspace-engine-sdk";
 import { useNavigate } from "react-router";
 
-import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
+import { Card, CardHeader, CardTitle } from "~/components/ui/card";
 import { useWorkspace } from "~/components/WorkspaceProvider";
-import { WorkflowTriggerForm } from "./WorkflowTriggerForm";
 
 type WorkflowTemplateCardProps = {
   workflowTemplate: WorkspaceEngine["schemas"]["WorkflowTemplate"];
