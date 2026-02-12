d5bcc9a1cbf3cb91152ef05a7b61a4d8d7c59337
Aditya Choudhari <adityachoudhari26@gmail.com>
2026-02-12 15:41:45 -0800

    remove dead code

--- apps/workspace-engine/pkg/workspace/jobagents/types/types.go
+++ apps/workspace-engine/pkg/workspace/jobagents/types/types.go
@@ -9,30 +9,3 @@ type Dispatchable interface {
 	Type() string
 	Dispatch(ctx context.Context, job *oapi.Job) error
 }
-
-// type DispatchContext struct {
-// 	Job            *oapi.Job               `json:"job"`
-// 	JobAgent       *oapi.JobAgent          `json:"jobAgent"`
-// 	JobAgentConfig oapi.JobAgentConfig     `json:"-"`
-// 	Release        *oapi.Release           `json:"release"`
-// 	Deployment     *oapi.Deployment        `json:"deployment"`
-// 	Environment    *oapi.Environment       `json:"environment"`
-// 	Resource       *oapi.Resource          `json:"resource"`
-// 	WorkflowRun    *oapi.WorkflowRun       `json:"workflowRun"`
-// 	WorkflowJob    *oapi.WorkflowJob       `json:"workflowJob"`
-// 	Version        *oapi.DeploymentVersion `json:"version"`
-// 	Inputs         map[string]any          `json:"inputs"`
-// }
-
-// func (r *DispatchContext) Map() map[string]any {
-// 	data, err := json.Marshal(r)
-// 	if err != nil {
-// 		return nil
-// 	}
-// 	var result map[string]any
-// 	if err := json.Unmarshal(data, &result); err != nil {
-// 		return nil
-// 	}
-
-// 	return result
-// }
