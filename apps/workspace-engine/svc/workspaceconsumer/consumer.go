1b2184142463cd2e3d7db742022c0c99389fb7a1
Aditya Choudhari <adityachoudhari26@gmail.com>
2026-03-02 15:59:48 -0800

    perf: disable db policies for now

--- apps/workspace-engine/svc/workspaceconsumer/consumer.go
+++ apps/workspace-engine/svc/workspaceconsumer/consumer.go
@@ -96,7 +96,7 @@ func (s *Service) configureManager() error {
 				wsstore.WithDBSystemEnvironments(bgCtx),
 				wsstore.WithDBResources(bgCtx),
 				wsstore.WithDBJobAgents(bgCtx),
-				wsstore.WithDBPolicies(bgCtx),
+				// wsstore.WithDBPolicies(bgCtx),
 				wsstore.WithDBPolicySkips(bgCtx),
 				wsstore.WithDBUserApprovalRecords(bgCtx),
 				wsstore.WithDBDeploymentVariables(bgCtx),
