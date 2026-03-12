40dd5e5af2a838b70e077be0ede940d862ba1ed0
Justin <justin@wandb.com>
2026-03-11 22:50:39 -0400

    refactor: remove Pebble persistence store and related test files

--- apps/workspace-engine/pkg/workspace/releasemanager/policy/evaluators.go
+++ apps/workspace-engine/pkg/workspace/releasemanager/policy/evaluators.go
@@ -3,36 +3,13 @@ package policy
 import (
 	"workspace-engine/pkg/oapi"
 	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/approval"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/deployableversions"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/deploymentdependency"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/deploymentwindow"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/environmentprogression"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/gradualrollout"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/versioncooldown"
-	"workspace-engine/pkg/workspace/releasemanager/policy/evaluator/versionselector"
 	"workspace-engine/pkg/workspace/store"
 )
 
 func EvaluatorsForPolicy(store *store.Store, rule *oapi.PolicyRule) []evaluator.Evaluator {
-	return evaluator.CollectEvaluators(
-		deployableversions.NewEvaluatorFromStore(store),
-		approval.NewEvaluatorFromStore(store, rule),
-		environmentprogression.NewEvaluatorFromStore(store, rule),
-		gradualrollout.NewEvaluatorFromStore(store, rule),
-		versionselector.NewEvaluator(rule),
-		deploymentdependency.NewEvaluatorFromStore(store, rule),
-		deploymentwindow.NewEvaluatorFromStore(store, rule),
-		versioncooldown.NewEvaluatorFromStore(store, rule),
-	)
+	return evaluator.CollectEvaluators()
 }
 
 func EvaluatorsForSummary(store *store.Store, rule *oapi.PolicyRule) []evaluator.Evaluator {
-	return evaluator.CollectEvaluators(
-		deploymentwindow.NewSummaryEvaluatorFromStore(store, rule),
-		approval.NewEvaluatorFromStore(store, rule),
-		environmentprogression.NewEvaluatorFromStore(store, rule),
-		gradualrollout.NewSummaryEvaluatorFromStore(store, rule),
-		versioncooldown.NewSummaryEvaluatorFromStore(store, rule),
-	)
+	return evaluator.CollectEvaluators()
 }
